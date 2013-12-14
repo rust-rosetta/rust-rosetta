1dbe44fa18e6b213e2fad4e6420c1a0af8b40295
Andrew Hobden <andrew@hoverbear.org>
2013-12-14 13:58:47 -0800

    Create a directory as well.

--- create_file.rs
+++ create_file.rs
@@ -1,10 +1,13 @@
 // Implements http://rosettacode.org/wiki/Create_a_file
 
-use std::io::File;
+use std::io::{File, fs};
+use std::libc::S_IRWXU;
 
 fn main () {
   // Create a new file.
   let mut new_file = File::create(&Path::new("output.txt")).unwrap();
   // Write something trivial to it.
-  new_file.write(bytes!("Nothing here..."))
+  new_file.write(bytes!("Nothing here..."));
+  // Create a directory.
+  fs::mkdir(&Path::new("docs"), S_IRWXU as u32)
 }
