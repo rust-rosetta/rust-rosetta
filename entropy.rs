81e8021fcc3953169e07675c62d3683443b193e9
thomas <thomas.poinsot1@gmail.com>
2014-01-13 16:23:19 +0100

    Made the shannon_entropy() method public for reuse, and upgraded precision to f64

--- entropy.rs
+++ entropy.rs
@@ -2,15 +2,15 @@ use std::num::Real;
 use std::hashmap::HashMap;
 use std::str::StrSlice;
 
-fn shannon_entropy(s: &str) -> f32 {
+pub fn shannon_entropy(s: &str) -> f64 {
     let mut map = HashMap::<char, uint>::new();
 
     for c in s.chars() {
         map.insert_or_update_with(c, 1, |_,v| *v += 1);
     }
 
-    map.iter().fold(0f32, |mut acc, (_, nb)| {
-        let p = (*nb as f32)/(s.len() as f32);
+    map.iter().fold(0f64, |mut acc, (_, nb)| {
+        let p = (*nb as f64)/(s.len() as f64);
         acc -= p * p.log2(); 
         acc
     })
