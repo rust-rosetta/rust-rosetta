995349248f6a09b68b20ab6e0b49353158897614
thomas <thomas.poinsot1@gmail.com>
2014-01-13 16:22:33 +0100

    Added a solution for the Fibonnaci word problem

---
+++ fibonacci_word.rs
@@ -0,0 +1,19 @@
+use entropy::shannon_entropy;
+mod entropy;
+
+fn main() {
+    println!("{:>2s}:{:>10s} {:s}", "N", "length", "entropy");
+
+    let mut previous = ~"1";
+    println!("{:>2i}:{:>10u} {:f}", 1, previous.len(), shannon_entropy(previous));
+
+    let mut next = ~"0";
+    println!("{:>2i}:{:>10u} {:f}", 2, next.len(), shannon_entropy(next));
+
+    for i in range(3, 38) {
+        let temp = next.clone();
+        next.push_str(previous);
+        previous = temp;
+        println!("{:>2i}:{:>10u} {:.15f}", i, next.len(), shannon_entropy(next));
+    }
+}
