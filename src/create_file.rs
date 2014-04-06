2747e208cff8ed2ab92a2ffe68ceac4d9e531340
Boris Egorov <egorov@linux.com>
2014-04-07 02:27:25 +0700

    Check file write result (removes compiler warning)

--- src/create_file.rs
+++ src/create_file.rs
@@ -9,7 +9,10 @@ fn main () {
   // Create a new file.
   let mut new_file = File::create(&Path::new("output.txt")).unwrap();
   // Write something trivial to it.
-  write!(&mut new_file as &mut Writer, "Nothing here...");
+  match writeln!(&mut new_file as &mut Writer, "Nothing here...") {
+      Ok(()) => (),
+      Err(e) => println!("failed to write to file: {}", e),
+  }
   // Create a directory.
   fs::mkdir(&Path::new("docs"), S_IRWXU as u32).ok();
 }
