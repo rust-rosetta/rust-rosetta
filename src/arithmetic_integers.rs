5e753ad4009a99f9d45617835c98b59d49993f58
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2014-04-10 14:34:13 +0200

    Add Artithmetic/Integer exemple

---
+++ src/arithmetic_integers.rs
@@ -0,0 +1,24 @@
+// http://rosettacode.org/wiki/Arithmetic/Integer
+
+use std::io::stdio::stdin;
+use std::from_str::from_str;
+use std::io::BufferedReader;
+
+fn main() {
+    let input = BufferedReader::new(stdin()).read_line().unwrap();
+    let mut words = input.words();
+    let a = match words.next().and_then(from_str::<int>) {
+            Some(_a) => _a,
+            _        => fail!("Please enter 2 integers")
+    };
+    let b = match words.next().and_then(from_str::<int>) {
+            Some(_b) => _b,
+            _        => fail!("Please enter 2 integers")
+    };
+
+    println!("a + b = {}", a + b);
+    println!("a - b = {}", a - b);
+    println!("a * b = {}", a * b);
+    println!("a / b = {}", a / b);
+    println!("a % b = {}", a % b);
+}
