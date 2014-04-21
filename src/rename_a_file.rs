7d08efac7ab91235e8c586de3dbdda52150dd7b4
Andrew Hobden <andrew@hoverbear.org>
2014-04-21 09:06:15 -0700

    Use unwrap instead of ok

--- src/rename_a_file.rs
+++ src/rename_a_file.rs
@@ -1,8 +1,8 @@
 use std::io::fs;
 
 fn main() {
-    fs::rename(&Path::new("input.txt"), &Path::new("output.txt")).ok();
-    fs::rename(&Path::new("docs"), &Path::new("mydocs")).ok();
-    fs::rename(&Path::new("/input.txt"), &Path::new("/output.txt")).ok();
-    fs::rename(&Path::new("/docs"), &Path::new("/mydocs")).ok();
+    fs::rename(&Path::new("input.txt"), &Path::new("output.txt")).unwrap();
+    fs::rename(&Path::new("docs"), &Path::new("mydocs")).unwrap();
+    fs::rename(&Path::new("/input.txt"), &Path::new("/output.txt")).unwrap();
+    fs::rename(&Path::new("/docs"), &Path::new("/mydocs")).unwrap();
 }
