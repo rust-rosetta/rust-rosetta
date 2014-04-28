7e4a877c946fc8f7d38bd3aa81a20a1e1900416e
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-04-28 20:59:22 +0200

    Improved arithmetic integers

--- src/arithmetic_integers.rs
+++ src/arithmetic_integers.rs
@@ -7,13 +7,10 @@ use std::io::BufferedReader;
 fn main() {
     let input = BufferedReader::new(stdin()).read_line().unwrap();
     let mut words = input.words();
-    let a = match words.next().and_then(from_str::<int>) {
-            Some(_a) => _a,
-            _        => fail!("Please enter 2 integers")
-    };
-    let b = match words.next().and_then(from_str::<int>) {
-            Some(_b) => _b,
-            _        => fail!("Please enter 2 integers")
+    
+    let (a, b) = match (words.next().and_then(from_str::<int>), words.next().and_then(from_str::<int>)) {
+            (Some(x), Some(y)) => (x, y),
+            _                  => fail!("Please enter 2 integers")
     };
 
     println!("a + b = {}", a + b);
