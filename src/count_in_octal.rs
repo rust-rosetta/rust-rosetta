28f1dade0bda1b0196ef5f95aba7f0bf7553d787
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-04-30 18:14:01 +0200

    Commented count in octal

--- src/count_in_octal.rs
+++ src/count_in_octal.rs
@@ -1,9 +1,11 @@
 // Implements http://rosettacode.org/wiki/Count_in_octal
 
-use std::uint;
+use std::u8;
+use std::iter::range_inclusive;
 
 fn main() {
-	for i in range(uint::MIN, uint::MAX) {
+    // We count from 0 to 255 (377 in octal)
+	for i in range_inclusive(0, u8::MAX) {
 		println!("{:o}", i);
 	}
 }
