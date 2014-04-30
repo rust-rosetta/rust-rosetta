9c6dc752dd5b4b772291db19ac7cba7222d56b77
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-04-30 18:03:54 +0200

    Fixed and commented callback to array

--- src/callback_to_array.rs
+++ src/callback_to_array.rs
@@ -1,11 +1,15 @@
 // Implements http://rosettacode.org/wiki/Apply_a_callback_to_an_array
 
 fn main () {
-  let an_array = [1,2,3,4,5];
-  println!("{:?}", an_array);
-  println!("{:?}", an_array.iter().map(callback));
+    let array = [1,2,3,4,5];
+    
+    println!("{}", array.as_slice());
+    
+    // The map does not mofify the array. It just returns an iterator.
+    // We iterate through the results of map and collect them into a vector.
+    println!("{}", array.iter().map(callback).collect::<Vec<int>>());
 }
 
 fn callback(val: &int) -> int {
-  val + 1
+    val + 1
 }
