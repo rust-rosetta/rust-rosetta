0c13ee03816bad5cbcbd642dbd9fd4c7a215cfd7
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-05-01 23:11:46 +0200

    Added test to gray code

--- src/gray_code.rs
+++ src/gray_code.rs
@@ -1,9 +1,11 @@
 // Implements http://rosettacode.org/wiki/Gray_code
 
+// Encode an uint
 fn gray_encode(integer: uint) -> uint {
 	(integer >> 1) ^ integer
 }
 
+// Decode an uint
 fn gray_decode(integer: uint) -> uint {
 	match integer {
 		0 => 0,
@@ -11,11 +13,15 @@ fn gray_decode(integer: uint) -> uint {
 	}
 }
 
-
+#[cfg(not(test))]
 fn main() {
 	for i in range(0u,32u) {
 		println!("{:2} {:0>5t} {:0>5t} {:2}", i, i, gray_encode(i),
 			gray_decode(i));
 	}
+}
 
+#[test]
+fn test_coherence() {
+    assert!(range(0u, 1000).all(|x| gray_decode(gray_encode(x)) == x));
 }
