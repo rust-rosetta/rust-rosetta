984f53f9a4e283abf5f0e2ac5e799e5f693b9cf0
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-05-03 18:10:42 +0200

    Added tests for palindrome and primality by trial division

--- src/palindrome.rs
+++ src/palindrome.rs
@@ -1,18 +1,35 @@
 // Implements http://rosettacode.org/wiki/Palindrome_detection
 
-// Simple character by character palindrome detector
+// Returns true if the string is a palindrome
 fn palindrome(string: &str) -> bool {
+    // The first part of the string
     let forward = string.chars().take(string.len() / 2);
-    let reverse = string.chars_rev().take(string.len() / 2);
+    
+    // The second part of the string in reverse order
+    let reverse = string.chars().rev().take(string.len() / 2);
+    
+    // We group the two parts of the string in tuples
     let mut both_directions = forward.zip(reverse);
     
+    // The word is a palindrome if each tuple contains two times
+    // the same character
     both_directions.all(|(a, b)| {a == b})
 }
 
 // Demonstration code
+#[cfg(not(test))]
 fn main() {
-    let test_strings = ["nope", "eevee", "lalala", "lalalal", "オオオオ"];
-    for string in test_strings.iter() {
-        println!("{:s} {:b}", *string, palindrome(*string));
+    let test_strings = ["nope", "eevee", "lalala", "rust", "lalalal"];
+    for &string in test_strings.iter() {
+        println!("{}: {}", string, palindrome(string));
     }
+}
+
+#[test]
+fn test_palindromes() {
+    let palindromes = ["eevee", "lalalal", "オオオオ", "", "anna"];
+    let non_palindromes = ["nope", "lalala", "car", "rain", "house", "computer", "rust"];
+    
+    assert!(palindromes.iter().all(|&s| palindrome(s)));
+    assert!(non_palindromes.iter().all(|&s| !palindrome(s)));
 }
--- src/primality_trial_div.rs
+++ src/primality_trial_div.rs
@@ -1,20 +1,36 @@
 //Implements http://rosettacode.org/wiki/Primality_by_Trial_Division
+
 use std::iter::range_step;
 
-fn is_prime(nb: int) -> bool {
-    if nb%2 == 0 {
+fn is_prime(number: int) -> bool {
+    if number % 2 == 0 && number != 2 {
         return false;
-    } else {
-        for i in range_step(3,(nb as f32).sqrt() as int + 1, 2) {
-            if nb%i == 0 {
-                return false;
-            }
-        }
     }
-    true
+
+    let limit = (number as f32).sqrt() as int + 1;
+    
+    // We test if the number is divisible by any odd number up to the limit
+    range_step(3, limit, 2).all(|x| number % x != 0)
 }
 
+#[cfg(not(test))]
 fn main() {
     println!("{:b}", is_prime(15485863)); // The 1 000 000th prime.
     println!("{:b}", is_prime(62773913)); // The product of the 1000th and 1001st primes.
+}
+
+#[test]
+fn test_one() {
+    assert!(is_prime(1));
+}
+
+#[test]
+fn test_two() {
+    assert!(is_prime(2));
+}
+
+#[test]
+fn test_many() {
+    let primes = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+    assert!(primes.iter().all(|&x| is_prime(x)));
 }
