4e4b931a871bb72f1c504fecf9f358d8a0fff26d
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-05-03 22:18:53 +0200

    Added tests to sieve eratosthenes

--- src/sieve_eratosthenes.rs
+++ src/sieve_eratosthenes.rs
@@ -1,29 +1,37 @@
 // Implements http://rosettacode.org/wiki/Sieve_of_Eratosthenes
 
-use std::iter;
+use std::iter::{range_inclusive, range_step};
 
 fn int_sqrt(n: uint) -> uint {
     (n as f64).sqrt() as uint
 }
 
-fn simple_sieve(limit: uint) -> ~[uint] {
+// Return the prime numbers up to limit
+fn simple_sieve(limit: uint) -> Vec<uint> {
     if limit < 2 {
-        return ~[];
+        return vec!();
     }
 
     let mut primes = Vec::from_fn(limit + 1, |_| true);
 
-    for prime in iter::range_inclusive(2, int_sqrt(limit) + 1) {
+    for prime in range_inclusive(2, int_sqrt(limit) + 1) {
         if *primes.get(0) {
-            for multiple in iter::range_step(prime * prime, limit + 1, prime) {
+            for multiple in range_step(prime * prime, limit + 1, prime) {
                 *primes.get_mut(multiple) = false
             }
         }
     }
     
-    iter::range_inclusive(2, limit).filter(|&n| *primes.get(n)).collect()
+    range_inclusive(2, limit).filter(|&n| *primes.get(n)).collect()
 }
 
+#[cfg(not(test))]
 fn main() {
-    println!("{:?}", simple_sieve(100))
+    println!("{}", simple_sieve(100))
+}
+
+#[test]
+fn test_basic() {
+    let primes = simple_sieve(30);
+    assert!(primes.as_slice() == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
 }
