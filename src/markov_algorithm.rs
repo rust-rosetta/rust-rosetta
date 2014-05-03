2f4213dd48912d8b9818a198199adab710a1091e
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-05-03 12:56:54 +0200

    Fixed markov algorithm and added test

--- src/markov_algorithm.rs
+++ src/markov_algorithm.rs
@@ -13,32 +13,27 @@ impl MarkovRule {
     }
 }
 
-// Struct for the complete markov algorithm
+// The complete markov algorithm
 struct MarkovAlgorithm {
     rules: Vec<MarkovRule>
 }
 
 impl MarkovAlgorithm {
-    
-    // parse an algorithm description to build a markov algorithm
-    pub fn build_from_string(s: &str) -> Option<MarkovAlgorithm> {
+    // Parse an algorithm description to build a markov algorithm
+    pub fn from_str(s: &str) -> Result<MarkovAlgorithm, ~str> {
         let mut rules: Vec<MarkovRule> = vec!();
-        for line in s.lines() {
-            // skip comment lines
-            if (line.char_len() > 0) && (line.char_at(0) == '#')
-            {
-                continue;
-            }
+        for line in s.lines()
+            .map(|l| l.trim()) // Ignore whitespace before and after
+            .filter(|l| l.char_len() > 0 && l.char_at(0) != '#') { // Ignore comments
             
             // check for -> (must be preceded by whitespace)
             // invalid ruleset if absent
             // whitespace rules mean there's 2 possible variations: " ->" and "\t->"
-            let arrow_pos = line.find_str(" ->").or_else(||{line.find_str("\t->")});
+            let arrow_pos = line.find_str(" ->").or_else(|| line.find_str("\t->"));
             match arrow_pos {
                 None => {
                     // Ruleset is invalid
-                    println!("Invalid rule \"{}\"", line);
-                    return None
+                    return Err(format!("Invalid rule \"{}\"", line));
                 }
                 Some(arrow) => {
                     // extract pattern (trim trailing whitespace)
@@ -61,10 +56,10 @@ impl MarkovAlgorithm {
             }
         }
         let rule_set = MarkovAlgorithm{rules: rules};
-        Some(rule_set)
+        Ok(rule_set)
     }
     
-    // transform a text string by applying the markov algorithm
+    // Transform a text string by applying the markov algorithm
     pub fn apply(&self, input: &str) -> ~str {
         
         // get a writable version of the input to work with
@@ -111,129 +106,136 @@ impl MarkovAlgorithm {
     }
 }
 
-// Demonstration of rosetta code samples
-
+// A Rosetta Code sample
 struct RCSample {
     ruleset: ~str,
     input: ~str,
     expected_result: ~str
 }
 
+// Sample markow algorithms from rosetta code
+// The extra whitespaces are trimmed when MarkovAlgorithm::from_str is called
+fn get_samples() -> [RCSample, ..5] {
+    [
+        RCSample {
+            ruleset:
+                "# This rules file is extracted from Wikipedia:
+                # http://en.wikipedia.org/wiki/Markov_Algorithm
+                A -> apple
+                B -> bag
+                S -> shop
+                T -> the
+                the shop -> my brother
+                a never used -> .terminating rule".to_owned(),
+            input: "I bought a B of As from T S.".to_owned(),
+            expected_result: "I bought a bag of apples from my brother.".to_owned()
+        },
+        RCSample{
+            ruleset:
+                "# Slightly modified from the rules on Wikipedia
+                A -> apple
+                B -> bag
+                S -> .shop
+                T -> the
+                the shop -> my brother
+                a never used -> .terminating rule".to_owned(),
+            input: "I bought a B of As from T S.".to_owned(),
+            expected_result: "I bought a bag of apples from T shop.".to_owned()
+        },
+        RCSample{
+            ruleset:
+                "# BNF Syntax testing rules
+                A -> apple
+                WWWW -> with
+                Bgage -> ->.*
+                B -> bag
+                ->.* -> money
+                W -> WW
+                S -> .shop
+                T -> the
+                the shop -> my brother
+                a never used -> .terminating rule".to_owned(),
+            input: "I bought a B of As W my Bgage from T S.".to_owned(),
+            expected_result: "I bought a bag of apples with my money from T shop.".to_owned()
+        },
+        RCSample{
+            ruleset:
+                "### Unary Multiplication Engine, for testing Markov Algorithm implementations
+                ### By Donal Fellows.
+                # Unary addition engine
+                _+1 -> _1+
+                1+1 -> 11+
+                # Pass for converting from the splitting of multiplication into ordinary
+                # addition
+                1! -> !1
+                ,! -> !+
+                _! -> _
+                # Unary multiplication by duplicating left side, right side times
+                1*1 -> x,@y
+                1x -> xX
+                X, -> 1,1
+                X1 -> 1X
+                _x -> _X
+                ,x -> ,X
+                y1 -> 1y
+                y_ -> _
+                # Next phase of applying
+                1@1 -> x,@y
+                1@_ -> @_
+                ,@_ -> !_
+                ++ -> +
+                # Termination cleanup for addition
+                _1 -> 1
+                1+_ -> 1
+                _+_ -> ".to_owned(),
+            input: "_1111*11111_".to_owned(),
+            expected_result: "11111111111111111111".to_owned()
+        },
+        RCSample{
+            ruleset:
+                "# Turing machine: three-state busy beaver
+                #
+                # state A, symbol 0 => write 1, move right, new state B
+                A0 -> 1B
+                # state A, symbol 1 => write 1, move left, new state C
+                0A1 -> C01
+                1A1 -> C11
+                # state B, symbol 0 => write 1, move left, new state A
+                0B0 -> A01
+                1B0 -> A11
+                # state B, symbol 1 => write 1, move right, new state B
+                B1 -> 1B
+                # state C, symbol 0 => write 1, move left, new state B
+                0C0 -> B01
+                1C0 -> B11
+                # state C, symbol 1 => write 1, move left, halt
+                0C1 -> H01
+                1C1 -> H11".to_owned(),
+            input: "000000A000000".to_owned(),
+            expected_result: "00011H1111000".to_owned()
+        },
+    ]
+}
+
+#[cfg(not(test))]
 fn main() {
-    // Sample markow algorithms from rosetta code
-    // Left aligned to avoid extra whitespace in strings
-    let samples = [
-        RCSample{
-            ruleset:
-~"# This rules file is extracted from Wikipedia:
-# http://en.wikipedia.org/wiki/Markov_Algorithm
-A -> apple
-B -> bag
-S -> shop
-T -> the
-the shop -> my brother
-a never used -> .terminating rule",
-            input: ~"I bought a B of As from T S.",
-            expected_result: ~"I bought a bag of apples from my brother."
-        },
-        RCSample{
-            ruleset:
-~"# Slightly modified from the rules on Wikipedia
-A -> apple
-B -> bag
-S -> .shop
-T -> the
-the shop -> my brother
-a never used -> .terminating rule",
-            input: ~"I bought a B of As from T S.",
-            expected_result: ~"I bought a bag of apples from T shop."
-        },
-        RCSample{
-            ruleset:
-~"# BNF Syntax testing rules
-A -> apple
-WWWW -> with
-Bgage -> ->.*
-B -> bag
-->.* -> money
-W -> WW
-S -> .shop
-T -> the
-the shop -> my brother
-a never used -> .terminating rule",
-            input: ~"I bought a B of As W my Bgage from T S.",
-            expected_result: ~"I bought a bag of apples with my money from T shop."
-        },
-        RCSample{
-            ruleset:
-~"### Unary Multiplication Engine, for testing Markov Algorithm implementations
-### By Donal Fellows.
-# Unary addition engine
-_+1 -> _1+
-1+1 -> 11+
-# Pass for converting from the splitting of multiplication into ordinary
-# addition
-1! -> !1
-,! -> !+
-_! -> _
-# Unary multiplication by duplicating left side, right side times
-1*1 -> x,@y
-1x -> xX
-X, -> 1,1
-X1 -> 1X
-_x -> _X
-,x -> ,X
-y1 -> 1y
-y_ -> _
-# Next phase of applying
-1@1 -> x,@y
-1@_ -> @_
-,@_ -> !_
-++ -> +
-# Termination cleanup for addition
-_1 -> 1
-1+_ -> 1
-_+_ -> ",
-            input: ~"_1111*11111_",
-            expected_result: ~"11111111111111111111"
-        },
-        RCSample{
-            ruleset:
-~"# Turing machine: three-state busy beaver
-#
-# state A, symbol 0 => write 1, move right, new state B
-A0 -> 1B
-# state A, symbol 1 => write 1, move left, new state C
-0A1 -> C01
-1A1 -> C11
-# state B, symbol 0 => write 1, move left, new state A
-0B0 -> A01
-1B0 -> A11
-# state B, symbol 1 => write 1, move right, new state B
-B1 -> 1B
-# state C, symbol 0 => write 1, move left, new state B
-0C0 -> B01
-1C0 -> B11
-# state C, symbol 1 => write 1, move left, halt
-0C1 -> H01
-1C1 -> H11",
-            input: ~"000000A000000",
-            expected_result: ~"00011H1111000"
-        },
-    ];
-    
-    for (index, sample) in samples.iter().enumerate() {
-        let markov_algorithm = MarkovAlgorithm::build_from_string(sample.ruleset);
-        
-        let output = markov_algorithm.map(|algorithm|{
-            algorithm.apply(sample.input)
-        });
-        
-        output.map(|output_string|{
-            println!("Sample {}", (index + 1))
-            println!("Expected: {}", sample.expected_result)
-            println!("Actual : {}", output_string);
-            assert!(sample.expected_result == output_string);
-        });
+    for (index, sample) in get_samples().iter().enumerate() {
+        match MarkovAlgorithm::from_str(sample.ruleset) {
+            Ok(algorithm) => {
+                println!("Sample {}", (index + 1));
+                println!("Output: {}", algorithm.apply(sample.input));
+            }
+            Err(message) => println!("{}", message)
+        }
+    }
+}
+
+#[test]
+fn test_samples() {
+    for sample in get_samples().iter() {
+        match MarkovAlgorithm::from_str(sample.ruleset) {
+            Ok(algorithm) => assert!(sample.expected_result == algorithm.apply(sample.input)),
+            Err(message) => fail!("{}", message)
+        }
     }
 }
