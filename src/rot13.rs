bcdb404966d5d46af361b713ea08d050d26775e8
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-05-03 21:47:28 +0200

    Added tests to rot13

--- src/rot13.rs
+++ src/rot13.rs
@@ -1,6 +1,6 @@
 // Implements http://rosettacode.org/wiki/Rot-13
 
-fn rot13 (string: ~str) -> ~str {
+fn rot13 (string: &str) -> ~str {
 	fn rot13u8 (c: u8) -> u8 {
 		match c {
 			97..109 => c+13,
@@ -11,11 +11,24 @@ fn rot13 (string: ~str) -> ~str {
 		}
 	}
     
-    let translated = string.as_bytes().iter().map(|c| rot13u8(*c)).collect();
+    let translated = string.as_bytes().iter().map(|&c| rot13u8(c)).collect();
 	std::str::from_utf8_owned(translated).unwrap()
 }
 
+#[cfg(not(test))]
 fn main () {
-	let a =  rot13(~"abc");
-	assert_eq!(a, ~"nop");
+    let string = "Do you love apples?";
+    
+    println!("Original: {}", string);
+    println!("Encoded: {}", rot13(string));
+}
+
+#[test]
+fn test_basic() {
+    assert!(rot13("abc") == ~"nop");
+}
+
+#[test]
+fn test_coherence() {
+    assert!(range(50000, 50050).all(|x| rot13(rot13(x.to_str())) == x.to_str()));
 }
