b2678b09abc404a980c61a64577a1e037f1a04ca
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-05-03 22:45:56 +0200

    Improved webserver

--- src/webserver.rs
+++ src/webserver.rs
@@ -5,16 +5,32 @@ use std::io::net::ip::{Ipv4Addr, SocketAddr};
 use std::io::{Acceptor, Listener};
 
 fn handle_client(mut stream: TcpStream) {
-    let response = bytes!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<doctype !html><html><head><title>Bye-bye baby bye-bye</title><style>body { background-color: #111 }h1 { font-size:4cm; text-align: center; color: black; text-shadow: 0 0 2mm red}</style></head><body><h1>Goodbye, world!</h1></body></html>\r\n");
+    let response = bytes!(
+"HTTP/1.1 200 OK
+Content-Type: text/html;
+charset=UTF-8
+
+<doctype !html>
+<html>
+    <head>
+        <title>Bye-bye baby bye-bye</title>
+        <style>
+            body { background-color: #111 }
+            h1 { font-size:4cm; text-align: center; color: black; text-shadow: 0 0 2mm red}
+        </style>
+    </head>
+    <body>
+        <h1>Goodbye, world!</h1>
+    </body>
+</html>");
     match stream.write(response) {
-        Ok(()) => println!("Response send!"),
+        Ok(_) => println!("Response sent!"),
         Err(e) => println!("Failed sending response: {}!", e),
     }
-    drop(stream);
 }
 
 fn main() {
-    let addr = SocketAddr { ip: Ipv4Addr(127, 0, 0, 1), port: 8080 };
+    let addr = SocketAddr { ip: Ipv4Addr(127, 0, 0, 1), port: 80 };
     let listener = TcpListener::bind(addr).unwrap();
 
     let mut acceptor = listener.listen();
@@ -23,8 +39,6 @@ fn main() {
     for stream in acceptor.incoming() {
         spawn(proc() {
             handle_client(stream.unwrap());
-        })
+        });
     }
-
-    drop(acceptor);
 }
