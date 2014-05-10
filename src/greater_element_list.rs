2eea4b55770bdbe4c0563d884a3c13f58dd16ed2
Florian MOREL <m.morel.florian@gmail.com>
2014-05-10 18:36:49 +0200

    improvement find max generic function + fixing README

--- src/greater_element_list.rs
+++ src/greater_element_list.rs
@@ -1,39 +1,43 @@
 // http://rosettacode.org/wiki/Greatest_element_of_a_list
 
-fn max_list(list : &[i32]) -> i32
-{
-	assert!(list.len() > 0,"Empty list not allowed");
-	let mut max = 0;
-	for &value in list.iter() {
-		//returns max of both value
-		max = std::cmp::max(max,value);
+/*
+	Generic function to find max value in list.
+	return an Option that can contains a reference to the max of the list
+
+	The type T requires to implement Ord to be compared.
+
+	'a is used to define the lifetime of the returned reference.
+	<'a, ...>: define a lifetime named 'a.
+	(lst: &'a Vec<T>): 'a is equal to the lifetime of lst
+	Option<&'a T>> the returned reference is defined to have the lifetime 'a, which is the same lifetime of the vector
+	This means that the reference contained in the Option will be valid as long as the vector is valid.
+	In fact, this mechanism exist to avoid having a refence to something that does not exist anymore. One of the Rust safety mechanism.
+
+	For more information about lifetime, see http://static.rust-lang.org/doc/master/guide-lifetimes.html
+*/
+fn max_list<'a, T: Ord>(lst: &'a Vec<T>) -> Option<&'a T> {
+
+	let mut max = None; //None is one of the Option values
+
+	for i in lst.iter() {
+		max = match max { //Max being Option(which is an enum), we deconstruct max to get its  possible values
+			None => Some(i), //if no max. some wrap i in an Option.
+			Some(ref m) if i > *m => Some(i), //m is a reference of the value contained in max
+			_ => max //default value if i <= max
+		}
 	}
-	return max;
-}
-
-//find greater element of a list generic
-//the type T must implement TotalOrd to sort
-//and Clone to clone the value from the list
-fn max_list_gen<T : TotalOrd + Clone>(list : &[T]) -> T
-{
-	assert!(list.len() > 0, "Empty list not allowed");
-
-	//need to clone the value, "list" being a immutable
-	let mut max = list[0].clone();
-
-	for n in range(1, list.len()) {
-		let value = list[n].clone();
-		//max of both value, require TotalOrd
-		max = std::cmp::max(max,value);
-	}
-	return max;
+	max
 }
 
 fn main()
 {
 	//greater element of a list main
-	println!("Max of first list {}", max_list([1,2,3,4,5,6,7]));
-	println!("Max of the second list {}", max_list([123,3543,23,432,5,2,34,234,234,2,4,234,23,4,24,25,7,658,68]));
-	println!("Max of list char {}", max_list_gen::<char>(['a','b','t','r','s',';','d','b']));
-	println!("Max of list int {}", max_list_gen::<int>([123,3543,23,432,5,2,34,234,234,2,4,234,23,4,24,25,7,658,68]));
+	let list1 = vec!(1,2,3,4,5,6,7,8,9);
+	println!("Max of first list {}", max_list(&list1).unwrap());
+
+	let list2 = vec!(123,3543,23,432,5,2,34,234,234,2,4,234,23,4,24,25,7,658,68);
+	println!("Max of the second list {}", max_list(&list2).unwrap());
+
+	let list3 = vec!('a','b','c','d','e');
+	println!("Max of first list {}", max_list(&list3).unwrap());
 }
