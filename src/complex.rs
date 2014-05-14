0337f4a4488b52599f4f52bff45c8b4960e1cd0a
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-05-14 22:18:12 +0200

    Fix Complex

--- src/complex.rs
+++ src/complex.rs
@@ -3,17 +3,17 @@
 
 extern crate num;
 
-use num::complex::Cmplx;
+use num::complex::Complex;
 
 fn main() {
-    let a = Cmplx::new(-4.0, 5.0);
-    let b = Cmplx::new(1.0, 1.0);
+    let a = Complex::new(-4.0, 5.0);
+    let b = Complex::new(1.0, 1.0);
 
     println!("a = {}", a);
     println!("b = {}", b);
     println!("a + b = {}", a + b);
     println!("a * b = {}", a * b);
-    println!("1 / a = {}", Cmplx::new(1.0, 0.0) / a);
+    println!("1 / a = {}", Complex::new(1.0, 0.0) / a);
     println!("-a = {}", -a);
     println!("conj a = {}", a.conj());
 }
