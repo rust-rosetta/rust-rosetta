15d182e09de1bc69884acbb0b6e088e5c35eac14
Matthijs Hofstra <thiezz@gmail.com>
2014-05-20 23:42:17 +0200

    Solution for String Interpolation

---
+++ src/string_interpolation.rs
@@ -0,0 +1,10 @@
+// Implements http://rosettacode.org/wiki/String_interpolation
+#![cfg(not_tested)]
+
+#[cfg(not(test))]
+fn main() {
+  let original = "Mary had a X lamb";
+  let little = "little";
+  let replaced = original.replace("X",little);
+  println!("{}",replaced);
+}
