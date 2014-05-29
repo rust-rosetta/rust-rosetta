847995513424fe2c2334fa341dac6376b2838e44
Paolo Falabella <paolo.falabella@gmail.com>
2014-05-29 18:16:24 +0100

    100 doors, added test

--- src/100_doors.rs
+++ src/100_doors.rs
@@ -1,9 +1,29 @@
 // Implements http://rosettacode.org/wiki/100_doors
-#![cfg(not_tested)]
+#[cfg(not(test))]
 fn main() {
-	for i in std::iter::range_inclusive(1,100) {
-		let x = (i as f64).powf(0.5);
-		let state = if x == x.round() {"open"} else {"closed"};
-		println!("Door {} is {}", i, state);
-	}
+    let drs = doors();
+    for (i, x) in drs.iter().enumerate() {
+        println!("Door {} is {}", i+1, x);
+    }
+}
+
+fn doors() -> Vec<&'static str> {
+   let mut ret = Vec :: with_capacity(100);
+   for i in std::iter::range_inclusive(1,100) {
+        let x = (i as f64).powf(0.5);
+        let state = if x == x.round() {"open"} else {"closed"};
+        ret.push(state);
+    }
+    ret
+}
+
+#[test]
+fn solution() {
+    let drs = doors();
+
+    // test that the doors with index corresponding to
+    // a perfect square are now open
+    for i in std::iter::range_inclusive(1u,10u) {
+        assert!(*drs.get(i*i - 1)=="open");
+    }
 }
