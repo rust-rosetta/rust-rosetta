d980fa679c46085601db67d1549db6aa94ca9b9f
Paolo Falabella <paolo.falabella@gmail.com>
2014-05-29 22:51:11 +0100

    binary digits, added tests

--- src/binary_digits.rs
+++ src/binary_digits.rs
@@ -1,8 +1,40 @@
 // Implements http://rosettacode.org/wiki/Binary_digits
-#![cfg(not_tested)]
+use std::vec::Vec;
+use std::string::String;
 
+#[cfg(not(test))]
 fn main() {
-    for i in range(0, 16) {
-        println!("{:t}", i)
+    let bins=binaries(16u);
+    for s in bins.iter() {
+        println!("{}", s);
     }
+}
+
+fn binaries(n:uint) -> Vec<String> {
+    let mut bins = Vec::<String>::with_capacity(n);
+    for i in range(0u, n) {
+        bins.push(format!("{:t}", i));
+    }
+    bins
+}
+
+#[test]
+fn test_digits() {
+    let bins=binaries(16u);
+    assert_eq!(bins, vec![String::from_str("0")
+                        ,String::from_str("1")
+                        ,String::from_str("10")
+                        ,String::from_str("11")
+                        ,String::from_str("100")
+                        ,String::from_str("101")
+                        ,String::from_str("110")
+                        ,String::from_str("111")
+                        ,String::from_str("1000")
+                        ,String::from_str("1001")
+                        ,String::from_str("1010")
+                        ,String::from_str("1011")
+                        ,String::from_str("1100")
+                        ,String::from_str("1101")
+                        ,String::from_str("1110")
+                        ,String::from_str("1111")]);
 }
