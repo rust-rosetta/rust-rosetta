e00bc745c70b0651e4e221c802825a2589053f23
Kevin Butler <haqkrs@gmail.com>
2014-05-31 23:52:04 +0100

    perfect_numbers: Stylistic updates.

--- src/perfect_numbers.rs
+++ src/perfect_numbers.rs
@@ -1,26 +1,27 @@
 // Implements http://rosettacode.org/wiki/Perfect_numbers
 
 fn perfect_number(n: uint) -> bool {
-  let upTo = (n/2)+1;
-  range(1,upTo).filter(|&i|n%i==0).fold(0,|s,u|s+u) == n
+  let limit = (n / 2) + 1;
+  let sum = range(1, limit).filter(|&i| n % i == 0)
+                           .fold(0, |sum, u| sum + u);
+  sum == n
 }
 
 #[cfg(not(test))]
 fn main() {
-  for n in range(2,10_000u) {
-    if perfect_number(n) {
-      println!("{}",n);
-    }
+  for n in range(2, 10_000u).filter(|&n| perfect_number(n)) {
+    println!("{}", n);
   }
 }
 
 #[test]
-fn testFirstFour() {
-  let nums = range(2,10_000u).filter(|&n|perfect_number(n)).collect::<Vec<uint>>();
-  assert_eq!(nums.as_slice(), &[6,28,496,8128]);
+fn test_first_four() {
+  let nums = range(2, 10_000u).filter(|&n| perfect_number(n))
+                              .collect::<Vec<uint>>();
+  assert_eq!(nums.as_slice(), &[6, 28, 496, 8128]);
 }
 
 #[test]
-fn testHighNumber() {
+fn test_high_number() {
   assert!( perfect_number(33550336) );
 }
