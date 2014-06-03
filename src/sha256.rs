29b3fc545062725712ff7bae0aecabfcd1bffa86
Paolo Falabella <paolo.falabella@gmail.com>
2014-06-03 22:03:18 +0100

    Implement sha256

---
+++ src/sha256.rs
@@ -0,0 +1,20 @@
+extern crate rustc;
+use rustc::util::sha2::{Sha256, Digest};
+
+#[cfg(not(test))]
+fn main() {
+    println!("{}", sha_256("Rosetta code"));
+}
+
+fn sha_256(input: &str) -> String {
+    let mut sh = box Sha256::new();
+    sh.input_str(input);       
+    sh.result_str()    
+}
+
+#[test]
+fn test_rosetta() {
+    assert_eq!(sha_256("Rosetta code"),
+    "764faf5c61ac315f1497f9dfa542713965b785e5cc2f707d6468d7d1124cdfcf"
+    .to_string());
+}
