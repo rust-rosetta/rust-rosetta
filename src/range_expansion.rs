861a7b4656cdaba205ba046aeec69ed98c309419
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-06-16 14:00:05 +0200

    Fix build

--- src/range_expansion.rs
+++ src/range_expansion.rs
@@ -49,7 +49,7 @@ fn test_basic() {
     assert!(expand_range(range).as_slice() == [1, 2, 3, 4, 5, 6]);
 
     let range = "-6,-3-1,3-5,7-11,14,15,17-20";
-    assert_eq!(expand_range(range).as_slice(),
+    assert!(expand_range(range).as_slice() ==
         [-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
 }
 
