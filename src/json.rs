d02c541c1d34b9c12d2ce097bfea869e353d7644
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-07-01 14:50:26 +0200

    Fix json

--- src/json.rs
+++ src/json.rs
@@ -2,7 +2,7 @@
 
 extern crate serialize;
 
-use serialize::{json, Encodable, Decodable};
+use serialize::json;
 
 #[deriving(Show, Encodable, Decodable, PartialEq, Eq)]
 pub struct Contact {
@@ -14,34 +14,24 @@ pub struct Contact {
 fn main() {
     // Encode contact to json
     let c = Contact { name: "John".to_str(), city: "Paris".to_str() };
-    let json = encode_contact(&c);
+    let json = json::encode(&c);
     println!("Encoded: {}", json.as_slice());
 
     // Decode json to contact
     let json_str = "{\"name\":\"Alan\", \"city\":\"Tokyo\"}";
-    let contact = decode_contact(json_str);
+    let contact: Contact = json::decode(json_str).unwrap();
     println!("Decoded: {}", contact);
-}
-
-fn encode_contact(data: &Contact) -> String {
-    json::Encoder::str_encode(&data)
-}
-
-fn decode_contact(json_str: &str) -> Contact {
-    let json_obj = json::from_str(json_str).unwrap();
-    let mut decoder = json::Decoder::new(json_obj);
-    Decodable::decode(&mut decoder).unwrap()
 }
 
 #[test]
 fn test_coherence() {
     let c = Contact { name: "John".to_str(), city: "Paris".to_str() };
-    assert!(decode_contact(encode_contact(&c).as_slice()) == c);
+    assert!(json::decode::<Contact>(json::encode(&c).as_slice()).unwrap() == c);
 }
 
 #[test]
 fn test_decode() {
     let json_str = "{\"name\":\"Alan\", \"city\":\"Tokyo\"}";
-    let contact = decode_contact(json_str);
+    let contact: Contact = json::decode(json_str).unwrap();
     assert!(contact == Contact { name: "Alan".to_str(), city: "Tokyo".to_str() });
 }
