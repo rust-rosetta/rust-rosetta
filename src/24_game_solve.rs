4c578c484f4edff759d8a9bae01e45b7a78bde5a
Paolo Falabella <paolo.falabella@gmail.com>
2014-07-04 07:31:15 +0100

    implementation of 24game/solve (squashed)

---
+++ src/24_game_solve.rs
@@ -0,0 +1,77 @@
+// http://rosettacode.org/wiki/24_game/Solve
+
+// modeled after the scala solution
+// http://rosettacode.org/wiki/24_game/Solve#Scala
+#![feature(macro_rules)]
+extern crate num;
+use num::rational::{Ratio, Rational};
+use std::num::Zero;
+// convenience macro to create a fixed-sized vector
+// of rationals by writing:
+// rational![1, 2, ...] instead of
+// [Ratio::<int>::from_integer(1), Ratio::<int>::from_integer(2), ...]
+macro_rules! rationals(
+    ($($e:expr),+) => ([$(Ratio::<int>::from_integer($e)),+])
+)
+#[cfg(not(test))]
+fn main() {
+    let mut r = rationals![1i, 3, 7, 9];
+    let sol = solve(r.as_mut_slice(), 24).unwrap_or("no solution found".to_string());
+    println!("{}", sol);
+}
+// for a vector of rationals r, find the combination of arithmentic
+// operations that yield target_val as a result (if such combination exists)
+fn solve(r: &mut[Rational], target_val: int) -> Option<String> {
+    //need to sort because next_permutation()
+    // returns permutations in lexicographic order
+    r.sort();
+    loop {
+        let all_ops = compute_all_operations(r);
+        for &(res, ref ops) in all_ops.iter() {
+            if res==Ratio::from_integer(target_val) {return Some(ops.to_string());}
+        }
+        if ! r.next_permutation() {return None;}
+    }
+}
+// applies all the valid combinations of + - * and / to the
+// numbers in l and for each combination creates a tuple
+// with the result and the expression in String form
+// returns all (result, expression in string form)
+// results in a vector
+fn compute_all_operations(l: &[Rational]) -> Vec<(Rational, String)> {
+    match l {
+        []         => vec![],
+        [x]  => vec![(x, (format!("{}", x)))],
+        [x,..rest] => {
+            let mut rt=Vec::new();
+            for &(y, ref exp) in compute_all_operations(rest).iter() {
+                let mut sub=vec![(x * y, "*"),(x + y, "+"), (x - y, "-")];
+                if !y.is_zero() {sub.push( (x/y, "/")); }
+                for &(z, ref op) in sub.iter() {
+                    let aux = (z, (format!("({} {} {})", x, op, exp )));
+                    rt.push(aux);
+                }
+            }
+            rt
+        }
+    }
+}
+#[test]
+fn test_rationals_macro() {
+    assert_eq!(
+    // without the rationals! macro
+    [Ratio::from_integer(1),
+    Ratio::from_integer(2),
+    Ratio::from_integer(3),
+    Ratio::from_integer(4)].as_slice(),
+    // with the rationals! macro
+    (rationals![1i, 2, 3, 4]).as_slice());
+}
+
+#[test]
+fn test_solve() {
+    let mut r = rationals![1i, 3, 7, 9];
+    assert_eq!(
+        solve(r.as_mut_slice(), 24),
+        Some("(9 / (3 / (1 + 7)))".to_string()));
+}
