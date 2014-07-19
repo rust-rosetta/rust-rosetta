0dc83182c04a244b5310bc2beae76c2f821f86b2
Kevin Butler <haqkrs@gmail.com>
2014-07-19 23:32:25 +0100

    anagrams: style

--- src/anagrams.rs
+++ src/anagrams.rs
@@ -1,66 +1,60 @@
 // Implements http://rosettacode.org/wiki/Anagrams
-
-use std::collections::{HashMap, HashSet};
-
 #[cfg(not(test))]
 use std::io::{File, BufferedReader};
-use std::cmp::max;
+use std::collections::{HashMap, HashSet};
 
-fn sort_string(string: &str) -> String {
-    let mut chars: Vec<char> = string.chars().collect();
+fn sorted_characters(string: &str) -> String {
+    let mut chars = string.chars().collect::<Vec<char>>();
     chars.sort();
     String::from_chars(chars.as_slice())
 }
 
-// Returns groups of anagrams where each group consists of a set
-// containing the words
-fn get_anagrams<T: Buffer>(mut reader: T) -> HashMap<String, HashSet<String>> {
+/// Returns groups of anagrams where each group consists of a set
+/// containing the words
+fn anagrams<T: Iterator<String>>(mut lines: T) -> HashMap<String, HashSet<String>> {
     let mut groups = HashMap::new();
 
     // Make groups of words according to the letters they contain
-    for line in reader.lines() {
-	let s = line.unwrap().as_slice().trim().to_string();
-	groups.insert_or_update_with(
-            // The key
-            sort_string(s.clone().as_slice()),
-            // The value
-            {
-                let mut set = HashSet::new();
-                set.insert(s.clone());
-                set
-            },
-            // The closure to update the value
-	    |_, group| { group.insert(s.clone()); }
-            );
+    for line in lines {
+        let s = line.as_slice().trim();
+        let set = groups.find_or_insert_with(// The key
+                                             sorted_characters(s),
+                                             // Insert new set if not found
+                                             |_| HashSet::new());
+
+        set.insert(s.to_string());
     }
 
     groups
 }
 
-// Returns the groups of anagrams that contain the most words in them
-fn get_biggest_groups(groups: &HashMap<String, HashSet<String>>)
+/// Returns the groups of anagrams that contain the most words in them
+fn largest_groups(groups: &HashMap<String, HashSet<String>>)
                       -> HashMap<String, HashSet<String>> {
-    let max_length = groups.iter()
-        .fold(0, |current_max, (_, group)| max(current_max,
-                                               group.len()));
-    groups.iter()
-        .filter(|&(_, group)| group.len() == max_length)
-        .map(|(x, y)| (x.clone(), y.clone()))
-        .collect()
+    let max_length = groups.iter().map(|(_, group)| group.len())
+                                  .max().unwrap();
+    groups.iter().filter_map(|(key, group)| {
+        if group.len() == max_length {
+            Some((key.clone(), group.clone()))
+        } else {
+            None
+        }
+    }).collect()
 }
 
 #[cfg(not(test))]
 fn main () {
     let path = Path::new("src/resources/unixdict.txt");
-    let reader = BufferedReader::new(File::open(&path));
+    let mut reader = BufferedReader::new(File::open(&path));
+    let lines = reader.lines().map(|l| l.unwrap());
 
-    let anagram_groups = get_anagrams(reader);
-    let biggest_groups = get_biggest_groups(&anagram_groups);
+    let anagram_groups = anagrams(lines);
+    let largest_groups = largest_groups(&anagram_groups);
 
-    // Print the words in the biggest groups of anagrams
-    for (_, group) in biggest_groups.iter() {
+    // Print the words in the largest groups of anagrams
+    for (_, group) in largest_groups.iter() {
         for word in group.iter() {
-            print!("{} ", *word)
+            print!("{} ", word)
         }
         println!("")
     }
@@ -68,44 +62,34 @@ fn main () {
 
 #[test]
 fn basic_test() {
-    use std::io::MemReader;
+    fn to_hash(s: &[&str]) -> HashSet<String> {
+        s.iter().map(|s| s.to_string()).collect()
+    };
 
-    // Groups of anagrams
-    let group1: HashSet<String> = vec!["lane".to_string(),
-                                       "neal".to_string(),
-                                       "lean".to_string()]
-                                       .move_iter().collect();
-    let group2: HashSet<String> = vec!["angel".to_string(),
-                                       "angle".to_string(),
-                                       "galen".to_string()]
-                                       .move_iter().collect();
-    let group3: HashSet<String> = vec!["glare".to_string(),
-                                       "large".to_string()]
-                                       .move_iter().collect();
-
-    // Prepare the input for the program
-    // We will get a string like "lane\nneal\nlean\nangel\nangle..."
-    let mut word_iter = group1.iter().chain(group2.iter().chain(group3.iter()));
-    let mut words = String::new();
-
-    words.push_str(word_iter.next().unwrap().as_slice());
-    for word in word_iter {
-        words.push_str("\n");
-        words.push_str(word.as_slice());
+    fn assert_has_value(map: &HashMap<String, HashSet<String>>,
+                        set: &HashSet<String>) {
+        assert!(map.values().any(|v| v == set));
     }
 
+    // Groups of anagrams
+    let group1 = &to_hash(&["lane", "neal", "lean"]);
+    let group2 = &to_hash(&["angel", "angle", "galen"]);
+    let group3 = &to_hash(&["glare", "large"]);
+
+    // Prepare the input for the program
+    let word_iter = group1.iter().chain(group2.iter().chain(group3.iter()));
+
     // Here begins the real testing
-    let all_groups = get_anagrams(MemReader::new(words.to_string().as_slice()
-                                                 .bytes().collect()));
-    let biggest_groups = get_biggest_groups(&all_groups);
+    let all_groups = &anagrams(word_iter.map(|s| s.to_string()));
+    let largest_groups = &largest_groups(all_groups);
 
     // Groups 1, 2 and 3 are contained in "all_groups"
-    assert!(all_groups.iter().any(|(_, group)| *group == group1));
-    assert!(all_groups.iter().any(|(_, group)| *group == group2));
-    assert!(all_groups.iter().any(|(_, group)| *group == group3));
+    assert_has_value(all_groups, group1);
+    assert_has_value(all_groups, group2);
+    assert_has_value(all_groups, group3);
 
-    // Groups 1 and 2 are contained in "biggest_groups". Group 3 is not.
-    assert!(biggest_groups.iter().any(|(_, group)| *group == group1));
-    assert!(biggest_groups.iter().any(|(_, group)| *group == group2));
-    assert!(biggest_groups.iter().all(|(_, group)| *group != group3));
+    // Groups 1 and 2 are contained in "largest_groups". Group 3 is not.
+    assert_has_value(largest_groups, group1);
+    assert_has_value(largest_groups, group2);
+    assert!(largest_groups.values().all(|group| group != group3));
 }
