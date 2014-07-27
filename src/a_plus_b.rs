45e4cda7e3a2fd087928751fb312d35867137aa7
Kevin Butler <haqkrs@gmail.com>
2014-07-27 23:01:52 +0100

    a + b: stdin is buffered

--- src/a_plus_b.rs
+++ src/a_plus_b.rs
@@ -1,19 +1,17 @@
 // Implements http://rosettacode.org/wiki/A%2BB
 // not_tested
 
-#![allow(unused_imports)]
-use std::io::stdio::stdin;
-use std::io::BufferedReader;
+use std::io::stdio;
 
 fn main() {
-    let input = BufferedReader::new(stdin()).read_line().unwrap();
+    let input = stdio::stdin().read_line().unwrap();
     let words = input.as_slice().words().take(2)
                             .map(from_str::<int>)
                             .collect::<Vec<Option<int>>>();
 
     let sum = match words.as_slice() {
         [Some(x), Some(y)] => x + y,
-		_                  => fail!("Please enter 2 integers")
+		    _ => fail!("Please enter 2 integers")
     };
 
     println!("{:i}", sum);
