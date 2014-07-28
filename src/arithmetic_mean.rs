b0c93626a3a84446a3ce7ac5ade69b3a2e5a2400
Kevin Butler <haqkrs@gmail.com>
2014-07-28 23:18:42 +0100

    arithmetic mean: unnecessary vecs

--- src/arithmetic_mean.rs
+++ src/arithmetic_mean.rs
@@ -14,21 +14,21 @@ fn mean(list: &[f64]) -> Option<f64> {
 
 #[cfg(not(test))]
 fn main() {
-    let input = vec!(3.0, 1.0, 4.0, 1.0, 5.0, 9.0);
+    let input = [3.0, 1.0, 4.0, 1.0, 5.0, 9.0];
 
     // This should be 3.833333
-    let mean = mean(input.as_slice()).unwrap();
+    let mean = mean(input).unwrap();
     println!("{}", mean);
 }
 
 #[test]
 fn simple_test() {
-    let vector = vec!(1.0, 2.0, 3.0, 4.0, 5.0);
-    assert!(mean(vector.as_slice()).unwrap() == 3.0);
+    let nums = [1.0, 2.0, 3.0, 4.0, 5.0];
+    assert_eq!(mean(nums), Some(3.0));
 }
 
 #[test]
 fn mean_empty_list() {
-    let empty_vec = vec!();
-    assert!(mean(empty_vec.as_slice()).is_none());
+    let no_nums = [];
+    assert_eq!(mean(no_nums), None);
 }
