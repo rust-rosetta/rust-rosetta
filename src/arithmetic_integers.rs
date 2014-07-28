742228b79cb46cc698ac5544ebe781cefe48bc8c
Kevin Butler <haqkrs@gmail.com>
2014-07-28 23:12:13 +0100

    arithmetic integers: style

--- src/arithmetic_integers.rs
+++ src/arithmetic_integers.rs
@@ -1,15 +1,16 @@
 // Implements http://rosettacode.org/wiki/Arithmetic/Integer
 // not_tested
+use std::io::stdin;
 
 fn main() {
-    let input = std::io::stdin().read_line().unwrap();
+    let input = stdin().read_line().unwrap();
     let words = input.as_slice().words().take(2)
-                            .map(from_str)
-                            .collect::<Vec<Option<int>>>();
+                                        .map(from_str)
+                                        .collect::<Vec<Option<int>>>();
 
     let (a, b) = match words.as_slice() {
             [Some(x), Some(y)] => (x, y),
-            _                  => fail!("Please enter 2 integers")
+            _ => fail!("Please enter 2 integers")
     };
 
     println!("a + b = {}", a + b);
