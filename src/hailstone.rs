4eee85ee6a734882cd491aabcd3fb1e7f0a81811
Kevin Butler <haqkrs@gmail.com>
2014-08-03 21:47:42 +0100

    hailstone: Simplify + style

--- src/hailstone.rs
+++ src/hailstone.rs
@@ -1,75 +1,69 @@
 // Implements http://rosettacode.org/wiki/Hailstone_sequence
 
-// Define a struct which we can make into an interator.
+// Define a struct which stores the state for the iterator.
 struct Hailstone {
-  current: uint, // Accessible online only to itself.
-  start:   uint  // Accessible to everything.
+    next: uint, // Accessible only to the current module.
+    pub start: uint  // Publically accessible.
 }
 
-// Define a constructor for the struct.
 impl Hailstone {
-  fn new(n: uint) -> Hailstone {
-    Hailstone { current: n, start: n }
-  }
+  // Define a constructor for the struct.
+    fn new(n: uint) -> Hailstone {
+        Hailstone { next: n, start: n }
+    }
 }
 
 // Implement the hailstone iteration sequence.
 impl Iterator<uint> for Hailstone {
-  // This gets called to fetch the next item of the iterator.
-  fn next(&mut self) -> Option<uint> {
-    let current = self.current;    // We need to cache the current value.
-    match current {
-      0               => {
-        // Resets the iterator.
-        self.current = self.start;
-        None
-      },
-      1               => {
-        // At the end, yield 1 and roll over next time called.
-        self.current = 0;
-        Some(1)
-      },
-      x if x % 2 == 0 => {
-        // Got an even.
-        self.current = x / 2;
+    // This gets called to fetch the next item of the iterator.
+    fn next(&mut self) -> Option<uint> {
+        // We need to cache the current value.
+        let current = self.next;
+        // And then calculate the 'next'
+        self.next = match current {
+            0 => {
+                // Resets the iterator.
+                self.next = self.start;
+                return None
+            },
+            // At the end, yield 1 and roll over next time called.
+            1 => 0,
+            // Got an even.
+            x if x % 2 == 0 => x / 2,
+            // All remaining numbers are odd.
+            x => (3 * x) + 1,
+        };
+
         Some(current)
-      },
-      x               => {
-        // All remaining numbers are odd.
-        self.current = (3 * x) + 1;
-        Some(current)
-      },
     }
-  }
 }
 
-// Returns the number and length of the longest hailstone sequence up to `limit`
+/// Returns the start number and length of the longest hailstone sequence up to `limit`
 fn biggest_hailstone(limit: uint) -> (uint, uint) {
-    let mut biggest = range(0u, limit).map(Hailstone::new)
-                                      .max_by(|&mut x| x.count())
-                                      .unwrap();
-    (biggest.start, biggest.count())
+    range(0u, limit).map(|n| (n, Hailstone::new(n).count()))
+                    .max_by(|&(_, count)| count)
+                    .unwrap()
 }
 
 #[cfg(not(test))]
 fn main() {
-  // Find the hailstone for 27.
-  let two_seven: Vec<uint> = Hailstone::new(27).collect();
-  let ts_len = two_seven.len();
-  println!("Testing: {}, Length: {}, Values: {}...{}",
-          two_seven[0],
-          ts_len,
-          two_seven.slice(0, 4),
-          two_seven.slice(ts_len - 4, ts_len));
+    // Find the hailstone for 27.
+    let two_seven = Hailstone::new(27).collect::<Vec<uint>>();
+    let ts_len = two_seven.len();
+    println!("Testing: {}, Length: {}, Values: {}...{}",
+            two_seven[0],
+            ts_len,
+            two_seven.slice(0, 4),
+            two_seven.slice(ts_len - 4, ts_len));
 
-  // Find the longest.
-  let (biggest, length) = biggest_hailstone(100000);
-  println!("Largest: {}, Size: {}", biggest, length);
+    // Find the longest.
+    let (biggest, length) = biggest_hailstone(100000);
+    println!("Largest: {}, Size: {}", biggest, length);
 }
 
 #[test]
 fn test_27() {
-    let seq: Vec<uint> = Hailstone::new(27).collect();
+    let seq = Hailstone::new(27).collect::<Vec<uint>>();
 
     assert_eq!(seq.slice(0, 4), &[27, 82, 41, 124]);
     assert_eq!(seq.slice(seq.len() - 4, seq.len()), &[8, 4, 2, 1]);
