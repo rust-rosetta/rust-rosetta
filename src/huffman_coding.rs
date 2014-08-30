9e8a6ff384b654b1d0c1bd31b128e6011b05d011
Paolo Falabella <paolo.falabella@gmail.com>
2014-08-30 19:20:56 +0200

    non snake case argument

--- src/huffman_coding.rs
+++ src/huffman_coding.rs
@@ -93,15 +93,15 @@ fn huffman_tree(input: &str) -> HNode {
 // its encoding string.
 fn build_encoding_table(tree: &HNode,
                       table: &mut HashMap<char,String>,
-                      startStr: &str) {
+                      start_str: &str) {
     match tree.item {
         HTree(ref data) => {
             build_encoding_table(&*data.left, table,
-                               String::from_str(startStr).append("0").as_slice());
+                               String::from_str(start_str).append("0").as_slice());
             build_encoding_table(&*data.right, table,
-                               String::from_str(startStr).append("1").as_slice());
+                               String::from_str(start_str).append("1").as_slice());
         },
-        HLeaf(ch)   => {table.insert(ch, String::from_str(startStr));}
+        HLeaf(ch)   => {table.insert(ch, String::from_str(start_str));}
     };
 }
 
