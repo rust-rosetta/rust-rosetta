a3720be66d2de2bd450a929947110d26fed447ef
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-09-16 11:01:44 +0200

    Fix filesize

--- src/filesize.rs
+++ src/filesize.rs
@@ -1,6 +1,8 @@
 // Implements http://rosettacode.org/wiki/File_size
 // not_tested
 
+use std::io::fs::PathExtensions;
+
 fn main() {
     let path_wd = Path::new("input.txt");
     println!("{}", path_wd.stat().unwrap().size);
