d80e7e2a82e40e1ab6bfd18bc2761653f931df1b
Hugh Manning <hugh_manning@yahoo.com>
2014-10-11 10:46:02 +1300

    Code cleanup based on aochagavia's suggestions

--- src/factor_int.rs
+++ src/factor_int.rs
@@ -1,5 +1,4 @@
 // Implements http://rosettacode.org/wiki/Factors_of_an_integer
-use std::num;
 
 #[cfg(not(test))]
 fn main() {
@@ -15,15 +14,10 @@ fn main() {
 // Compute the factors of an integer
 // This method uses a simple check on each value between 1 and sqrt(x) to find
 // pairs of factors
-fn factor_int(x: int) -> Box<Vec<int>> {
+fn factor_int(x: int) -> Vec<int> {
     let mut factors: Vec<int> = Vec::new();
     
-    // Compute the largest value to check i.e. sqrt(x)
-    // Since we expect sane values rather than user input, we just fail if the
-    // type conversions don't work
-    let x_f: f64 = num::from_int(x).expect("Math fail!");
-    let bound_f = x_f.sqrt();
-    let bound: int = num::from_f64(bound_f.floor()).expect("Math fail!");
+    let bound: int = (x as f64).sqrt().floor() as int;
     
     for i in range(1i, bound) {
         if x % i == 0 {
@@ -32,11 +26,11 @@ fn factor_int(x: int) -> Box<Vec<int>> {
         }
     }
     
-    box factors
+    factors
 }
 
 #[test]
 fn test() {
     let result = factor_int(78i);
-    assert_eq!(*result, vec![1i, 78, 2, 39, 3, 26, 6, 13]);
+    assert_eq!(result, vec![1i, 78, 2, 39, 3, 26, 6, 13]);
 }
