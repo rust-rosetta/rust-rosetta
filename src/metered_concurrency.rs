aa7d5dfbeb46f32f2873b107136aa9f54fb8c4ac
Joshua Yanovski <pythonesque@gmail.com>
2014-10-10 00:12:55 -0700

    Adding better comments.

--- src/metered_concurrency.rs
+++ src/metered_concurrency.rs
@@ -1,3 +1,4 @@
+// Implements http://rosettacode.org/wiki/Metered_concurrency
 // Rust has a perfectly good Semaphore type already.  It lacks count(), though, so we can't use it
 // directly.
 #![feature(unsafe_destructor)]
@@ -16,27 +17,38 @@ pub struct CountingSemaphore {
 }
 
 pub struct CountingSemaphoreGuard<'a> {
-    sem: &'a CountingSemaphore,
+    sem: &'a CountingSemaphore, // A reference to the owning semaphore.
 }
 
 impl CountingSemaphore {
+    // Create a semaphore with `max` available resources and a linearly increasing backoff of
+    // `backoff` (used during spinlock contention).
     pub fn new(max: uint, backoff: Duration) -> CountingSemaphore {
         CountingSemaphore { count: AtomicUint::new(max), backoff: backoff }
     }
 
+    // Acquire a resource, returning a RAII CountingSemaphoreGuard.
     pub fn acquire(&self) -> CountingSemaphoreGuard {
-        // Spinlock, more or less
+        // Spinlock until remaining resource count is at least 1
+        let mut backoff: Duration = self.backoff;
         loop {
+            // Probably don't need SeqCst here, but it doesn't hurt.
             let count = self.count.load(atomics::SeqCst);
+            // The check for 0 is necessary to make sure we don't go negative, which is why this
+            // must be a compare-and-swap rather than a straight decrement.
             if count == 0 || self.count.compare_and_swap(count, count - 1, atomics::SeqCst) != count {
-                timer::sleep(self.backoff);
+                // Linear backoff a la Servo's spinlock contention.
+                timer::sleep(backoff);
+                backoff = backoff + self.backoff;
             } else {
+                // We successfully acquired the resource.
                 break
             }
         }
         CountingSemaphoreGuard { sem: self }
     }
 
+    // Return remaining resource count
     pub fn count(&self) -> uint {
         self.count.load(atomics::SeqCst)
     }
@@ -51,17 +63,17 @@ impl<'a> Drop for CountingSemaphoreGuard<'a> {
 }
 
 fn metered(duration: Duration, backoff: Duration) {
-    static MAX_COUNT: uint = 4;
-    static NUM_THREADS: u8 = 10;
+    static MAX_COUNT: uint = 4; // Total available resources
+    static NUM_WORKERS: u8 = 10; // Number of workers contending for the resources
     let sem = Arc::new(CountingSemaphore::new(MAX_COUNT, backoff));
     let (tx, rx) = channel();
-    for i in range(0, NUM_THREADS) {
+    for i in range(0, NUM_WORKERS) {
         let sem = sem.clone();
         let tx = tx.clone();
         spawn(proc() {
             let guard = sem.acquire();
             let count = sem.count();
-            assert!(count <= MAX_COUNT);
+            assert!(count < MAX_COUNT);
             println!("Worker {} after acquire: count = {}", i, count);
             timer::sleep(duration);
             drop(guard);
@@ -72,7 +84,7 @@ fn metered(duration: Duration, backoff: Duration) {
         })
     }
     drop(tx);
-    for _ in range(0, NUM_THREADS) {
+    for _ in range(0, NUM_WORKERS) {
         rx.recv();
     }
 }
