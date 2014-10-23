48af3433b6ece75979ce395da4c2a2e0232047d0
Rupert Lane <rupert@rupert-lane.org>
2014-10-23 19:39:59 +0700

    Implemented leap_year

---
+++ src/leap_year.rs
@@ -0,0 +1,33 @@
+// http://rosettacode.org/wiki/Leap_year
+
+fn is_leap_year(year: int) -> bool {
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+}
+
+#[cfg(not(test))]
+fn main () {
+    for &year in [1900, 1995, 1996, 1999, 2000, 2001].iter() {
+        println!("{} {} a leap year", year,
+                 if is_leap_year(year) { "is" } else { "is not" });
+    }
+}
+
+#[test]
+fn test_regular_common_year() {
+    assert_eq!(is_leap_year(2014), false);
+}
+
+#[test]
+fn test_regular_leap_year() {
+    assert_eq!(is_leap_year(2012), true);
+}
+
+#[test]
+fn test_century_common_year() {
+    assert_eq!(is_leap_year(1900), false);
+}
+
+#[test]
+fn test_400_leap_year() {
+    assert_eq!(is_leap_year(2000), true);
+}
