b74ba361b94970bb73c55ac4d7cb7980f7dc0e6b
Paolo Falabella <paolo.falabella@gmail.com>
2014-10-26 17:41:09 +0100

    main not tested (fix warning)
    added #cfg[not(test)] on main

--- src/horners_rule.rs
+++ src/horners_rule.rs
@@ -3,7 +3,8 @@ use std::num::zero;
 fn horner<T:Num>(cs:&[T], x:T) -> T {
     cs.iter().rev().fold(zero::<T>(), |acc, c| (acc*x) + (*c))
 }
- 
+
+#[cfg(not(test))] 
 fn main() {
     println!("{}", horner([-19i, 7, -4, 6], 3i)); // 128
 }
