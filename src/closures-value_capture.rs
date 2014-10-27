93f1929446337378675fb61b0bad070ee396b5c2
Adolfo Ochagavía <aochagavia92@gmail.com>
2014-10-27 17:07:50 +0100

    Add Closures/Value capture

---
+++ src/closures-value_capture.rs
@@ -0,0 +1,18 @@
+// http://rosettacode.org/wiki/Closures/Value_capture
+
+#![feature(overloaded_calls, unboxed_closures)]
+
+use std::iter::count;
+
+fn main() {
+    // An infinite iterator that generates closures
+    let closures = count(0u, 1).map(|x| move |:| (x as f64).powi(2));
+
+    // Take the first 9 closures from the iterator and call them
+    for c in closures.take(9) {
+        println!("{}", c())
+    }
+}
+
+// FIXME: add a function that returns an iterator over unboxed closures
+// (blocked by the lack of anonymous types)
