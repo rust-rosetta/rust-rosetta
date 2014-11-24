89c29410e64e13eaa7e7e0fe628f3aeacd314f73
James Hurst <jamesrhurst@outlook.com>
2014-11-24 14:17:07 -0500

    Pascal's triangle: added test

--- src/pascals_triangle.rs
+++ src/pascals_triangle.rs
@@ -1,22 +1,53 @@
 // Implements http://rosettacode.org/wiki/Pascal%27s_triangle
 
-fn pascaltriangle(rows: uint) {
+fn pascaltriangle(rows: uint) -> Vec<Vec<uint>> {
+    let mut all_rows = Vec::with_capacity(rows);
+
     for row in range(0, rows) {
+        let mut row_vals = Vec::with_capacity(row + 1);
         let mut value = 1;
 
-        for _ in range(0, (rows - row)) {
-            print!(" ");
-        }
-
         for col in range(0, row + 1) {
-            print!("{} ", value);
+            row_vals.push(value);
             value = value * (row - col)/(col + 1)
         }
 
+        all_rows.push(row_vals);
+    }
+
+    all_rows
+}
+
+#[cfg(not(test))]
+fn printpascal(rows: &Vec<Vec<uint>>) {
+    let mut i = 0;
+    for row in rows.iter() {
+        for _ in range(0, (rows.len() - i)) {
+            print!(" ");
+        }
+
+        for col in row.iter() {
+            print!("{} ", col);
+        }
+
         println!("");
+
+        i += 1;
     }
 }
 
+#[cfg(not(test))]
 fn main() {
-    pascaltriangle(5);
+    printpascal(&pascaltriangle(5));
+}
+
+#[test]
+fn test_triangle() {
+    assert_eq!(pascaltriangle(5), vec!(
+                  vec!(1),
+                 vec!(1, 1),
+               vec!(1, 2, 1),
+              vec!(1, 3, 3, 1),
+             vec!(1, 4, 6, 4, 1)
+            ));
 }
