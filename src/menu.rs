3fc9d3473f4fa2f8a38062be9f70b62e745e5da3
Rootul Patel <rootulp@gmail.com>
2014-12-26 19:01:31 -0500

    implement changes from comments by aochagavia

--- src/menu.rs
+++ src/menu.rs
@@ -3,46 +3,31 @@
 use std::io;
 
 // Print the menu followed by the prompt
-fn print_both(menu: [&str, ..4], prompt: &str) {
+fn print_both(menu: &[&str], prompt: &str) {
 
-    // Iterate through array and print index, period, menu item
-    for i in range(0, 4u) { // Hard coded number 4 here
-        println!("{}. {}", i, menu[i]);
+    // Iterate through array and print index, period, and menu item
+    for (i, item) in menu.iter().enumerate() {
+        println!("{}. {}", i, item);
     }
 
     // Print the prompt
     println!("{}", prompt);
-
 }
+
 
 // Grab the next line of input
-fn next_input() -> int {
+fn next_input() -> Option<uint> {
 
-    // Convert it to a possible int
-    let mut stdin = io::stdin();
-    let line: io::IoResult<String> = stdin.read_line();
-    let string: String = line.unwrap();
-    let trimmed: &str = string.as_slice().trim();
-    let possible: Option<int> = from_str(trimmed);
-
-    // If possible num is between 0 and 3, return it. 
-    // Otherwise return -1 <= I don't think this is good practice
-    match possible {
-        None => return -1,
-        Some(possible) => match possible {
-            0...3 => return possible,
-            _=> return -1
-        }
-    }
+    let line = io::stdin().read_line().unwrap();
+    let input: Option<uint> = line.trim().parse();
+    return input;
 }
 
-// Couldn't figure out how to make select return an element of menu
-// Got a 'missing lifetime specifier' error when trying to return &str
-fn select(menu: [&str, ..4], prompt: &str) -> String {
-
+fn select<'a>(menu: &'a [&str], prompt: &str) -> &'a str {
+    
     // Check if menu is empty
     if menu.len() == 0 {
-        return "".to_string()
+        return "";
     }
 
     // Loop until user inputs a valid menu index
@@ -50,20 +35,36 @@ fn select(menu: [&str, ..4], prompt: &str) -> String {
 
         print_both(menu, prompt);
 
-        let menu_index: int = next_input();
+        let input = next_input();
 
-        match menu_index{
-            0...3 => return menu[menu_index as uint].to_string(),
-            _=> print_both(menu, prompt),
+        let num = match input {
+            Some(num) => num,
+            None      => continue
+        };
+
+        if let 0...3 = num {
+            return menu[num];
         }
     }
 }
 
 #[cfg(not(test))]
 fn main() {
-    let items = ["fee fie", "huff and puff", "mirror mirror", "tick tock"];
+
     let prompt = "Choose one.";
-    println!("{}", select(items, prompt));
+    let items = ["fee fie", "huff and puff", "mirror mirror", "tick tock"];
+    let menu = items.slice(0,4);
+
+    println!("{}", select(menu, prompt));
 }
 
-// Need to add tests but having trouble simulating std io
+
+#[test]
+fn test_empty_menu() {
+    let prompt = "Choose one.";
+    let items = ["fee fie", "huff and puff", "mirror mirror", "tick tock"];
+    let menu = items.slice(0,0);
+    assert_eq!(select(menu, prompt), "");
+}
+
+// Need to add more tests but having trouble simulating std input
