6f716357b0b8f366d09b3b894d17111684995508
Paolo Falabella <paolo.falabella@gmail.com>
2015-01-11 18:32:29 +0100

    json

--- src/json.rs
+++ src/json.rs
@@ -1,11 +1,7 @@
 // Implements http://rosettacode.org/wiki/JSON
-
-// work around 
-// https://github.com/nikomatsakis/rust/commit/c61a0092bc236c4be4cb691fcd50ff50e91ab0d6
-#![feature(old_orphan_check)] 
+#![allow(unstable)]
 
 extern crate "rustc-serialize" as rustc_serialize;
-
 use rustc_serialize::json;
 
 #[derive(Show, RustcEncodable, RustcDecodable, PartialEq, Eq)]
