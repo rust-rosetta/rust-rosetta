94e4a1ce0272dcbcc346fd70f48a029dbd193f31
Bartosz Marcinkowski <bm371613@gmail.com>
2015-01-18 18:19:34 +0100

    added Josephus problem

---
+++ src/josephus_problem.rs
@@ -0,0 +1,30 @@
+// Implements http://rosettacode.org/wiki/Josephus_problem
+#![allow(unstable)]
+
+// implementation based on observation:
+// `josephus(n, k) == if n < 2 { 0 } else { (josephus(n - 1, k) + k) % n }`
+fn josephus(n: u32, k: u32) -> u32 {
+    let mut result = 0u32;
+    for i in 2..(n+1) {
+        result = (result + k) % i;
+    };
+    result
+}
+
+#[cfg(not(test))]
+fn main() {
+    let n = 5;
+    let k = 2;
+    println!("n: {} k: {} survivor: {}", n, k, josephus(n, k));
+}
+
+#[cfg(test)]
+mod test {
+    use super::josephus;
+
+    #[test]
+    fn test() {
+        assert!(josephus(5, 2) == 2);
+        assert!(josephus(41, 3) == 30);
+    }
+}
