28ab8b2d859f70c4ba3c15ff2a895c73bf1b9779
Yale Cason <yale.cason@gmail.com>
2015-01-22 01:47:16 -0500

    Added simple Proper Divisors implementation

---
+++ src/proper_divisors.rs
@@ -0,0 +1,24 @@
+// Implements http://rosettacode.org/wiki/Proper_divisors
+
+fn proper_divisors(n:i32) -> Vec<i32>{
+	(1..(n / 2)+1).filter(|&i| n % i == 0).collect()
+}
+
+#[cfg(not(test))]
+fn main() {
+	for n in 1..11{
+		println!("{:?}", proper_divisors(n));
+	}
+
+	let max_divisors = (1..20001)
+		.map(|i| proper_divisors(i).len())
+		.max().unwrap();
+
+	println!("{}", max_divisors);
+}
+
+#[test]
+fn test_divisors() {
+    assert!(proper_divisors(6) == vec!(1,2,3));
+    assert!(proper_divisors(100) == vec!(1,2,4,5,10,20,25,50));
+}
