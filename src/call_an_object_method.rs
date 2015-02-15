6589f12091b314434e82c6c0d467afab3f98bc00
glendc <decauwsemaecker.glen@gmail.com>
2015-02-15 21:21:12 +0000

    adding call_an_object_to_method example for static/instance methods

---
+++ src/call_an_object_method.rs
@@ -0,0 +1,28 @@
+// Implements http://rosettacode.org/wiki/Call_an_object_method
+
+struct Foo;
+
+impl Foo {
+    // implementation of an instance method for struct Foo
+    // returning the answer to life
+    fn get_the_answer_to_life(&self) -> i32 {
+        42
+    }
+
+    // implementation of a static method for struct Foo
+    // returning a new instance object
+    fn new() -> Foo {
+        println!("Hello, world!");
+        Foo // returning the new Foo object
+    }
+}
+
+fn main() {
+    // create the instance object foo,
+    // by calling the static method new of struct Foo
+    let foo = Foo::new();
+
+    // get the answer to life 
+    // by calling the instance method of object foo
+    println!("The answer to life is {}.", foo.get_the_answer_to_life());
+}
