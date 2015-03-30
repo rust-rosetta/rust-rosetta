2faeafe1e90395ec0b85980486f508850adbddde
Adolfo Ochagavía <aochagavia92@gmail.com>
2015-03-30 21:53:35 +0200

    Add support for non-ASCII characters in longest_common_subsequence

--- src/longest_common_subsequence.rs
+++ src/longest_common_subsequence.rs
@@ -1,14 +1,15 @@
 // Implements http://rosettacode.org/wiki/Longest_common_subsequence
-#![feature(collections)]
-
 
 /// Returns the longest common subsequence of a and b.
 fn longest_common_subsequence(a: &str, b: &str) -> String {
-    let mut lengths = vec![vec![0; b.len() + 1]; a.len() + 1];
-    
-    for i in 0..a.len() {
-        for j in 0..b.len() {
-            if a.char_at(i) == b.char_at(j) {
+    let a_chars: Vec<_> = a.chars().collect();
+    let b_chars: Vec<_> = b.chars().collect();
+
+    let mut lengths = vec![vec![0; b_chars.len() + 1]; a_chars.len() + 1];
+
+    for (i, a_char) in a_chars.iter().enumerate() {
+        for (j, b_char) in b_chars.iter().enumerate() {
+            if a_char == b_char {
                 lengths[i + 1][j + 1] = lengths[i][j] + 1;
             } else {
                 lengths[i + 1][j + 1] = std::cmp::max(
@@ -17,9 +18,9 @@ fn longest_common_subsequence(a: &str, b: &str) -> String {
         }
     }
 
-    let mut lcs = String::new();
-    let mut i = a.len();
-    let mut j = b.len();
+    let mut lcs = Vec::new();
+    let mut i = a_chars.len();
+    let mut j = b_chars.len();
 
     while i != 0 && j != 0 {
         if lengths[i][j] == lengths[i - 1][j] {
@@ -27,13 +28,13 @@ fn longest_common_subsequence(a: &str, b: &str) -> String {
         } else if lengths[i][j] == lengths[i][j - 1] {
             j -= 1;
         } else {
-            lcs.push(a.char_at(i - 1));
+            lcs.push(a_chars[i - 1]);
             i -= 1;
             j -= 1;
         }
     }
 
-    lcs.chars().rev().collect()
+    lcs.into_iter().rev().collect()
 }
 
 
@@ -59,4 +60,10 @@ fn test_longest_common_subsequence() {
     assert_eq!(longest_common_subsequence("abc", ""), "");
     assert_eq!(longest_common_subsequence("abcdefg", "abc"), "abc");
     assert_eq!(longest_common_subsequence("aaaa", "aaaaaa"), "aaaa");
+}
+
+#[test]
+fn test_unicode() {
+    assert_eq!(longest_common_subsequence("самолетов была отмечена в Японском", "отмечена в"),
+               "отмечена в");
 }
