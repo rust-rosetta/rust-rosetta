4c3876e2a1ea6f6baa3200fb53e2e502cb1fddb7
Mathieu David <mathieudavid@mathieudavid.org>
2015-06-01 16:22:50 +0200

    Implemented the suggestion of @aochagavia that is a lot easier to read and is also a lot shorter too

--- src/empty_directory.rs
+++ src/empty_directory.rs
@@ -1,31 +1,26 @@
 // http://rosettacode.org/wiki/Empty_directory
 use std::fs;
-use std::path::Path;
 use std::env::args;
 
 fn main() {
 
     // Open path
-    let path = if let Some(arg) = args().nth(1) {
+    let dir = if let Some(arg) = args().nth(1) {
         arg
     } else {
         println!("You must give a path as argument");
         return;
     };
 
-    let dir = Path::new(&path);
-
-    let paths = match fs::read_dir(dir) {
-        Err(e) => {
-            println!("Error: {}", e);
-            return;
-        },
-        Ok(f) => f,
-    };
-
-    if paths.count() > 0 {
-        println!("Directory is not empty");
-    } else {
-        println!("Directory is empty");
+    // map function for type Result executes a function (closure)
+    // on Ok(_) types but leaves Err(_) untouched.
+    //
+    // The directory is empty if the result of paths.count() == Ok(0)
+    // In any other case, the directory is not empty
+    //
+    match fs::read_dir(dir).map(|paths| paths.count()) {
+        Err(e) => println!("Error: {}", e),
+        Ok(0) => println!("Directory is empty"),
+        Ok(_) => println!("Directory is not empty")
     }
 }
