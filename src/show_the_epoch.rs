4efd85d7da3fe6adf0752a128bc6671cd23e6a75
Andy Russell <arussell123@gmail.com>
2015-11-11 17:05:08 -0500

    add "Show the epoch"

---
+++ src/show_the_epoch.rs
@@ -0,0 +1,9 @@
+// http://rosettacode.org/wiki/Show_the_epoch
+extern crate time;
+
+use time::{at_utc, Timespec};
+
+fn main() {
+    let epoch = at_utc(Timespec::new(0, 0));
+    println!("{}", epoch.asctime());
+}
