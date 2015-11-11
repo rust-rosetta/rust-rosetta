d4fcc2457277d1016f1798a92d94ef7b2ed28cff
Andy Russell <arussell123@gmail.com>
2015-11-11 16:05:02 -0500

    add "Loops/For with a specified step"

---
+++ src/loops/for_with_a_specified_step.rs
@@ -0,0 +1,9 @@
+// http://rosettacode.org/wiki/Loops/For_with_a_specified_step
+#![feature(step_by)]
+
+fn main() {
+    for i in (2..8+1).step_by(2) {
+        print!("{}", i);
+    }
+    println!("who do we appreciate?!");
+}
