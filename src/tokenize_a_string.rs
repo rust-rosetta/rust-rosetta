4f07da5259827dfba01ebf05d6cd84062645ba5f
Boris Egorov <egorov@linux.com>
2016-01-15 14:10:57 +0600

    Implement "Tokenize a string"

---
+++ src/tokenize_a_string.rs
@@ -0,0 +1,7 @@
+// http://rosettacode.org/wiki/Tokenize_a_string
+
+fn main() {
+    let s = "Hello,How,Are,You,Today";
+    let tokens: Vec<&str> = s.split(",").collect();
+    println!("{}", tokens.join("."));
+}
