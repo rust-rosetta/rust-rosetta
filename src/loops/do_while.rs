549845dedb6608c9c8264366c4f1337905430bc1
Andy Russell <arussell123@gmail.com>
2016-02-18 22:39:28 -0500

    add "Loops/Do-while"

---
+++ src/loops/do_while.rs
@@ -0,0 +1,17 @@
+// http://rosettacode.org/wiki/Loops/Do-while
+
+//! Rust does not have a `do...while` loop. Instead, the keyword `loop` is used with a termination
+//! condition.
+
+fn main() {
+    let mut x = 0;
+
+    loop {
+        x += 1;
+        println!("{}", x);
+
+        if x % 6 == 0 {
+            break;
+        }
+    }
+}
