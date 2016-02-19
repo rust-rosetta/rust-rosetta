22f9e30680778e5b58b496c6f71727d66d205aaf
Andy Russell <arussell123@gmail.com>
2016-02-18 21:42:45 -0500

    add "Here document"

---
+++ src/here_document.rs
@@ -0,0 +1,10 @@
+// http://rosettacode.org/wiki/Here_document
+
+fn main() {
+    // Similar to C++, Rust offers raw strings:
+    let x = r#"
+        This is a "raw string literal," roughly equivalent to a heredoc.
+    "#;
+
+    println!("{}", x);
+}
