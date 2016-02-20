f8d38c3117d79386da1f3a406eb5387b0b2069d9
Andy Russell <arussell123@gmail.com>
2016-02-20 16:48:25 -0500

    simplify command line interface

--- coverage/src/main.rs
+++ coverage/src/main.rs
@@ -1,17 +1,19 @@
+#![feature(plugin)]
+#![plugin(docopt_macros)]
+
 extern crate coverage;
 extern crate difference;
 extern crate docopt;
-extern crate rustc_serialize;
 extern crate rust_rosetta;
+extern crate rustc_serialize;
 extern crate term;
 
-use docopt::Docopt;
 use difference::Difference;
 use term::Terminal;
 
 use std::io::prelude::*;
 
-const USAGE: &'static str = r"
+docopt!(Args derive Debug, "
 Detect unimplemented tasks.
 
 This script prints out the name of each task, followed by whether it is implemented online,
@@ -22,34 +24,51 @@ If no tasks are specified, determines the status for all tasks.
 Optionally prints out a diff as well.
 
 Usage:
-    coverage [options] [--localonly | --remoteonly | --unimplemented] [<tasks>...]
+    coverage [options] [<tasks>...]
 
 Options:
     -h --help           Show this screen.
-    --nodiff            Don't print diffs.
-    --localonly         Only print tasks that are implemented locally, but not on the wiki.
-    --remoteonly        Only print tasks that are implemented on the wiki, but not locally.
-    --unimplemented     Only print tasks that neither implemented locally nor remotely.
-";
 
-#[derive(Debug, RustcDecodable)]
-struct Args {
-    arg_tasks: Vec<String>,
-    flag_nodiff: bool,
-    flag_localonly: bool,
-    flag_remoteonly: bool,
-    flag_unimplemented: bool,
+    --nodiff            Don't print diffs.
+
+    --filter=<type>     Filter tasks printed by the program. Accepted values:
+
+                            all                 Print all tasks (default).
+
+                            localonly           Only print tasks that are implemented locally, but
+                                                not on the wiki.
+
+                            remoteonly          Only print tasks that are implemented on the wiki,
+                                                but not locally.
+
+                            unimplemented       Only print tasks that neither implemented locally
+                                                nor remotely.
+", flag_filter: Option<TaskFilter>);
+
+#[derive(Debug, Clone, RustcDecodable)]
+enum TaskFilter {
+    All,
+    LocalOnly,
+    RemoteOnly,
+    Unimplemented,
+}
+
+impl Default for TaskFilter {
+    fn default() -> Self {
+        TaskFilter::All
+    }
 }
 
 fn main() {
-    let args: Args = Docopt::new(USAGE)
-                         .and_then(|d| d.decode())
+    let args: Args = Args::docopt()
+                         .decode()
                          .unwrap_or_else(|e| e.exit());
 
     let mut t = term::stdout().unwrap();
 
     let tasks = if args.arg_tasks.len() > 0 {
         coverage::fetch_tasks(&args.arg_tasks
+                                   .clone()
                                    .iter()
                                    .map(|title| title.to_owned())
                                    .collect::<Vec<_>>())
@@ -57,11 +76,14 @@ fn main() {
         coverage::fetch_all_tasks()
     };
 
+    let task_filter = args.flag_filter.unwrap_or_default().to_owned();
+
     for task in tasks {
-        if (args.flag_localonly && !task.is_local_only()) ||
-           (args.flag_remoteonly && !task.is_remote_only()) ||
-           (args.flag_unimplemented && !task.is_unimplemented()) {
-            continue;
+        match task_filter {
+            TaskFilter::LocalOnly if !task.is_local_only() => continue,
+            TaskFilter::RemoteOnly if !task.is_remote_only() => continue,
+            TaskFilter::Unimplemented if !task.is_unimplemented() => continue,
+            TaskFilter::All | _ => (),
         }
 
         t.attr(term::Attr::Bold).unwrap();
