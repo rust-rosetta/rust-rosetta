3ffdae712357f058223844e86a73d73c98d78034
Andy Russell <arussell123@gmail.com>
2016-02-20 16:20:05 -0500

    refactor crate logic into library

---
+++ coverage/src/lib.rs
@@ -0,0 +1,176 @@
+#[macro_use]
+extern crate lazy_static;
+
+extern crate hyper;
+extern crate regex;
+extern crate rust_rosetta;
+extern crate url;
+extern crate walkdir;
+
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::iter::FromIterator;
+use std::path::PathBuf;
+
+use hyper::Client;
+use hyper::header::Connection;
+use regex::Regex;
+use url::Url;
+use walkdir::WalkDir;
+
+use rust_rosetta::rosetta_code::find_unimplemented_tasks;
+
+lazy_static!{
+    static ref TASK_COMMENT_RE: Regex = Regex::new("// http://rosettacode.org/wiki/(.+)").unwrap();
+    static ref LIB_OR_MOD: Regex = Regex::new("^(lib|mod)$").unwrap();
+
+    /// Extracts code from the first Rust section from Rosetta Code wiki markup.
+    static ref RUST_WIKI_SECTION_RE: Regex =
+        Regex::new(r"==\{\{header\|Rust\}\}==(?s:.*?)<lang rust>((?s:.*?))</lang>").unwrap();
+
+}
+
+pub struct Task {
+    /// The title of the task.
+    pub title: String,
+
+    /// The code of the task implemented locally. If the tasks is not implemented in the
+    /// repository, then it will be `None`.
+    pub local_code: Option<String>,
+
+    /// The code of the task implemented on the Rosetta Code wiki. If the task is not yet
+    /// implemented online, it will be `None`.
+    pub remote_code: Option<String>,
+}
+
+impl Task {
+    /// True if and only if the task is only implemented on the Rosetta Code wiki.
+    pub fn is_remote_only(&self) -> bool {
+        self.local_code.is_none() && self.remote_code.is_some()
+    }
+
+    /// True if and only if the task is is only implemented in the repository.
+    pub fn is_local_only(&self) -> bool {
+        self.local_code.is_some() && self.remote_code.is_none()
+    }
+
+    /// True if and only if the task is neither implemented on the Rosetta code wiki or on the
+    /// repository.
+    pub fn is_unimplemented(&self) -> bool {
+        self.local_code.is_none() && self.local_code.is_none()
+    }
+}
+
+/// Transforms a task title to a URL task title.
+fn normalize(title: &str) -> String {
+    title.replace(" ", "_")
+}
+
+/// Returns the titles of every task on Rosetta Code.
+fn all_task_titles() -> Vec<String> {
+    find_unimplemented_tasks::all_tasks().iter().map(|task| task.title.to_owned()).collect()
+}
+
+/// Iterator over task information. One `Task` will be returned each iteration.
+pub struct TaskIterator {
+    task_titles: VecDeque<String>,
+    local_tasks: HashMap<String, PathBuf>,
+    client: Client,
+}
+
+impl TaskIterator {
+    pub fn new(titles: &[String]) -> Self {
+        // Determine which tasks are implemented locally by walking the src folder and reading the
+        // comment at the top of the file.
+        let mut local_tasks = HashMap::new();
+        for entry in WalkDir::new("../src") {
+            let entry = entry.unwrap();
+            let file_stem = entry.path().file_stem().unwrap().to_str().unwrap();
+
+            // If we find a non-Rust file (or a lib or mod) skip it.
+            match entry.path().extension().and_then(|s| s.to_str()) {
+                Some("rs") if !LIB_OR_MOD.is_match(file_stem) => (),
+                _ => continue,
+            }
+
+            let file = File::open(entry.path()).unwrap();
+            let first_line = BufReader::new(file).lines().next().unwrap().unwrap();
+            let task_name = TASK_COMMENT_RE.captures(&first_line)
+                                           .and_then(|c| c.at(1))
+                                           .expect(&format!("could not parse task name for {:?}",
+                                                            entry.path()));
+
+            local_tasks.insert(task_name.to_owned(), entry.path().to_owned());
+        }
+
+        TaskIterator {
+            client: Client::new(),
+            task_titles: titles.iter().cloned().collect(),
+            local_tasks: local_tasks,
+        }
+    }
+}
+
+impl Iterator for TaskIterator {
+    type Item = Task;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.task_titles.pop_front().map(|title| {
+            let normalized_title = normalize(&title);
+
+            let mut task_url = Url::parse(&format!("http://rosettacode.org/wiki/{}",
+                                                   normalized_title))
+                                   .unwrap();
+            task_url.set_query_from_pairs(&[("action", "raw")]);
+
+
+            let local_code = self.local_tasks
+                                 .get(&normalized_title)
+                                 .and_then(|path| File::open(path).ok())
+                                 .map(|mut file| {
+                                     let mut local_code = String::new();
+                                     file.read_to_string(&mut local_code).unwrap();
+                                     local_code
+                                 });
+
+
+            let mut res = self.client
+                              .get(&task_url.serialize())
+                              .header(Connection::close())
+                              .send()
+                              .unwrap();
+
+            let mut body = String::new();
+            res.read_to_string(&mut body).unwrap();
+            let remote_code = RUST_WIKI_SECTION_RE.captures(&body)
+                                                  .map(|captures| captures.at(1).unwrap())
+                                                  .map(|code| code.to_owned());
+
+            Task {
+                title: title.to_owned(),
+                local_code: local_code,
+                remote_code: remote_code,
+            }
+        })
+    }
+}
+
+/// Retrieves data for every task on Rosetta Code.
+pub fn fetch_all_tasks() -> TaskIterator {
+    fetch_tasks(&all_task_titles())
+}
+
+/// Parses both local (implemented in this repository) and remote (implemented on the wiki) tasks,
+/// and returns the code of each.
+pub fn fetch_tasks(tasks: &[String]) -> TaskIterator {
+    let all_task_titles: HashSet<String> = HashSet::from_iter(all_task_titles());
+    let requested_task_titles = HashSet::from_iter(tasks.iter().cloned());
+    let mut task_titles: Vec<String> = all_task_titles.intersection(&requested_task_titles)
+                                                      .cloned()
+                                                      .collect();
+    task_titles.sort();
+
+    TaskIterator::new(&task_titles)
+}
--- coverage/src/main.rs
+++ coverage/src/main.rs
@@ -1,27 +1,15 @@
+extern crate coverage;
 extern crate difference;
 extern crate docopt;
-extern crate hyper;
-extern crate term;
-extern crate regex;
-extern crate rust_rosetta;
 extern crate rustc_serialize;
-extern crate walkdir;
-
-use rust_rosetta::rosetta_code::find_unimplemented_tasks::all_tasks;
-
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::path::PathBuf;
+extern crate rust_rosetta;
+extern crate term;
 
 use docopt::Docopt;
 use difference::Difference;
-use hyper::Client;
-use hyper::header::Connection;
-use regex::Regex;
 use term::Terminal;
-use walkdir::WalkDir;
+
+use std::io::prelude::*;
 
 const USAGE: &'static str = r"
 Detect unimplemented tasks.
@@ -53,11 +41,6 @@ struct Args {
     flag_unimplemented: bool,
 }
 
-/// Transforms a task title to a URL task title.
-fn normalize(title: &str) -> String {
-    title.replace(" ", "_")
-}
-
 fn main() {
     let args: Args = Docopt::new(USAGE)
                          .and_then(|d| d.decode())
@@ -65,90 +48,35 @@ fn main() {
 
     let mut t = term::stdout().unwrap();
 
-    let http_client = Client::new();
-
-    // Determine which tasks are implemented locally by walking the src folder and reading the
-    // comment at the top of the file.
-    let mut local_tasks: HashMap<String, PathBuf> = HashMap::new();
-    let task_comment = Regex::new("// http://rosettacode.org/wiki/(.+)").unwrap();
-    for entry in WalkDir::new("../src") {
-        let entry = entry.unwrap();
-        let lib_or_mod = Regex::new("^lib|mod$").unwrap();
-        let file_stem = entry.path().file_stem().unwrap().to_str().unwrap();
-
-        // If we find a non-Rust file (or a lib or mod) skip it.
-        match entry.path().extension().and_then(|s| s.to_str()) {
-            Some("rs") if !lib_or_mod.is_match(file_stem) => (),
-            _ => continue,
-        }
-
-        let file = File::open(entry.path()).unwrap();
-        let first_line = BufReader::new(file).lines().next().unwrap().unwrap();
-        let task_name = task_comment.captures(&first_line)
-                                    .and_then(|c| c.at(1))
-                                    .expect(&format!("could not parse task name for {:?}",
-                                                     entry.path()));
-
-        local_tasks.insert(task_name.to_owned(), entry.path().to_owned());
-    }
-
-    let task_titles: Vec<String> = if args.arg_tasks.len() > 0 {
-        args.arg_tasks
+    let tasks = if args.arg_tasks.len() > 0 {
+        coverage::fetch_tasks(&args.arg_tasks
+                                   .iter()
+                                   .map(|title| title.to_owned())
+                                   .collect::<Vec<_>>())
     } else {
-        all_tasks().iter().cloned().map(|t| t.title).collect()
+        coverage::fetch_all_tasks()
     };
 
-    // Extracts the code from the first <lang rust> tag
-    let rust_re = Regex::new(r"==\{\{header\|Rust\}\}==(?s:.*?)<lang rust>((?s:.*?))</lang>")
-                      .unwrap();
-
-    for title in task_titles {
-        let task_url = &format!("http://rosettacode.org/wiki/{}", normalize(&title));
-
-        let local_code = local_tasks.get(&normalize(&title))
-                                    .and_then(|path| File::open(path).ok())
-                                    .and_then(|mut file| {
-                                        let mut local_code = String::new();
-                                        file.read_to_string(&mut local_code).unwrap();
-                                        Some(local_code)
-                                    });
-
-
-        let mut res = http_client.get(&format!("{}?{}", task_url, "action=raw"))
-                                 .header(Connection::close())
-                                 .send()
-                                 .unwrap();
-
-        let mut body = String::new();
-        res.read_to_string(&mut body).unwrap();
-        let online_code = rust_re.captures(&body)
-                                 .and_then(|captures| Some(captures.at(1).unwrap()));
-
-        if args.flag_localonly && !(local_code.is_some() && online_code.is_none()) {
-            continue;
-        }
-
-        if args.flag_remoteonly && !(local_code.is_none() && online_code.is_some()) {
-            continue;
-        }
-
-        if args.flag_unimplemented && (local_code.is_some() || online_code.is_some()) {
+    for task in tasks {
+        if (args.flag_localonly && !task.is_local_only()) ||
+           (args.flag_remoteonly && !task.is_remote_only()) ||
+           (args.flag_unimplemented && !task.is_unimplemented()) {
             continue;
         }
 
         t.attr(term::Attr::Bold).unwrap();
-        writeln!(t, "{}", title).unwrap();
+        writeln!(t, "{}", task.title).unwrap();
         t.reset().unwrap();
 
         writeln!(t,
                  "Local: {}, Remote: {}",
-                 local_code.is_some(),
-                 online_code.is_some())
+                 task.local_code.is_some(),
+                 task.remote_code.is_some())
             .unwrap();
 
-        if !args.flag_nodiff && online_code.is_some() && local_code.is_some() {
-            let (_dist, changeset) = difference::diff(&online_code.unwrap(),
-                                                      &local_code.unwrap(),
+        if !args.flag_nodiff && task.remote_code.is_some() && task.local_code.is_some() {
+            let (_dist, changeset) = difference::diff(&task.remote_code.unwrap(),
+                                                      &task.local_code.unwrap(),
                                                       "\n");
 
             let mut t = term::stdout().unwrap();
