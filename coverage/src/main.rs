d4e22fd8fed8810e4bb31dc7ad4caa91673cea0f
Andy Russell <arussell123@gmail.com>
2016-02-20 17:50:57 -0500

    display "✔" or "✘" instead of "true" or "false"

--- coverage/src/main.rs
+++ coverage/src/main.rs
@@ -8,10 +8,13 @@ extern crate rust_rosetta;
 extern crate rustc_serialize;
 extern crate term;
 
+use std::io::prelude::*;
+use std::io;
+
 use difference::Difference;
 use term::Terminal;
 
-use std::io::prelude::*;
+use coverage::Task;
 
 docopt!(Args derive Debug, "
 Detect unimplemented tasks.
@@ -59,6 +62,80 @@ impl Default for TaskFilter {
     }
 }
 
+/// Prints a colored diff of two strings to the terminal.
+fn print_diff<T: ?Sized>(t: &mut T, s1: &str, s2: &str) -> io::Result<()>
+    where T: Terminal
+{
+    let (_dist, changeset) = difference::diff(&s1, &s2, "\n");
+
+    for i in 0..changeset.len() {
+        match changeset[i] {
+            Difference::Same(ref x) => {
+                try!(t.reset());
+                try!(writeln!(t, " {}", x));
+            }
+            Difference::Add(ref x) => {
+                try!(t.fg(term::color::GREEN));
+                for line in x.split("\n") {
+                    try!(writeln!(t, "+{}", line));
+                }
+            }
+            Difference::Rem(ref x) => {
+                try!(t.fg(term::color::RED));
+                for line in x.split("\n") {
+                    try!(writeln!(t, "-{}", line));
+                }
+            }
+        }
+    }
+    try!(t.reset());
+    try!(t.flush());
+    Ok(())
+}
+
+/// Prints a task in a human-readable format.
+fn print_task<T: ?Sized>(t: &mut T, task: &Task, diff: bool) -> io::Result<()>
+    where T: Terminal
+{
+    try!(t.attr(term::Attr::Bold));
+    try!(writeln!(t, "{}", task.title));
+    try!(t.reset());
+
+    try!(write!(t, "Local:"));
+    try!(write_status(t, task.local_code.is_some()));
+
+    try!(write!(t, "Remote:"));
+    try!(write_status(t, task.remote_code.is_some()));
+    try!(writeln!(t, ""));
+
+    if let (Some(ref local_code), Some(ref remote_code)) = (task.local_code.clone(),
+                                                            task.remote_code.clone()) {
+        if diff {
+            try!(print_diff(t, remote_code, local_code));
+        }
+    }
+
+    Ok(())
+}
+
+/// Writes a boolean as a pretty, human-readable string.
+fn write_status<T: ?Sized>(t: &mut T, boolean: bool) -> io::Result<()>
+    where T: Terminal
+{
+    try!(t.attr(term::Attr::Bold));
+
+    if boolean {
+        try!(t.fg(term::color::GREEN));
+        try!(write!(t, " ✔ "))
+    } else {
+        try!(t.fg(term::color::RED));
+        try!(write!(t, " ✘ "));
+    }
+
+    try!(t.reset());
+    Ok(())
+}
+
 fn main() {
     let args: Args = Args::docopt()
                          .decode()
@@ -83,48 +160,7 @@ fn main() {
             TaskFilter::LocalOnly if !task.is_local_only() => continue,
             TaskFilter::RemoteOnly if !task.is_remote_only() => continue,
             TaskFilter::Unimplemented if !task.is_unimplemented() => continue,
-            TaskFilter::All | _ => (),
+            TaskFilter::All | _ => print_task(&mut *t, &task, !args.flag_nodiff).unwrap(),
         }
-
-        t.attr(term::Attr::Bold).unwrap();
-        writeln!(t, "{}", task.title).unwrap();
-        t.reset().unwrap();
-
-        writeln!(t,
-                 "Local: {}, Remote: {}",
-                 task.local_code.is_some(),
-                 task.remote_code.is_some())
-            .unwrap();
-
-        if !args.flag_nodiff && task.remote_code.is_some() && task.local_code.is_some() {
-            let (_dist, changeset) = difference::diff(&task.remote_code.unwrap(),
-                                                      &task.local_code.unwrap(),
-                                                      "\n");
-
-            let mut t = term::stdout().unwrap();
-
-            for i in 0..changeset.len() {
-                match changeset[i] {
-                    Difference::Same(ref x) => {
-                        t.reset().unwrap();
-                        writeln!(t, " {}", x).unwrap();
-                    }
-                    Difference::Add(ref x) => {
-                        t.fg(term::color::GREEN).unwrap();
-                        for line in x.split("\n") {
-                            writeln!(t, "+{}", line).unwrap();
-                        }
-                    }
-                    Difference::Rem(ref x) => {
-                        t.fg(term::color::RED).unwrap();
-                        for line in x.split("\n") {
-                            writeln!(t, "-{}", line).unwrap();
-                        }
-                    }
-                }
-            }
-        }
-        t.reset().unwrap();
-        t.flush().unwrap();
     }
 }
