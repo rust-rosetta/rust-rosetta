656d36f77f2e51a407ce3621590b2690b41cb821
Andy Russell <arussell123@gmail.com>
2016-03-07 18:52:31 -0500

    use inclusive range syntax

--- src/multiplication_tables.rs
+++ src/multiplication_tables.rs
@@ -1,24 +1,22 @@
 // http://rosettacode.org/wiki/Multiplication_tables
 
-#![feature(range_inclusive)]
-
-use std::iter::range_inclusive;
+#![feature(inclusive_range_syntax)]
 
 const LIMIT: i32 = 12;
 
 fn main() {
-    for i in range_inclusive(1, LIMIT) {
+    for i in 1...LIMIT {
         print!("{:3} ", i);
     }
     print!("\n");
 
-    for i in 0..LIMIT {
+    for _ in 0..LIMIT {
         print!("----");
     }
     print!("+\n");
 
-    for i in range_inclusive(1, LIMIT) {
-        for j in range_inclusive(1, LIMIT) {
+    for i in 1...LIMIT {
+        for j in 1...LIMIT {
             if j < i {
                 print!("    ")
             } else {
