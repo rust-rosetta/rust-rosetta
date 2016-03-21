f15eb23754db7a74af503666be9c75c7f4a83d8f
Dillon Tracy <dillon.tracy@gmail.com>
2016-03-21 02:28:13 -0500

    o all tests passing o added bench, iterator o rustfmt

--- src/bernoulli_numbers.rs
+++ src/bernoulli_numbers.rs
@@ -1,5 +1,8 @@
 // http://rosettacode.org/wiki/Bernoulli_numbers
 
+// Relative speeds of optimized, iterator, and naive implementations is
+// 625:25:1
+
 #![feature(test)]
 
 extern crate num;
@@ -7,31 +10,40 @@ extern crate test;
 
 use num::bigint::{BigInt, ToBigInt};
 use num::rational::{BigRational};
+use std::cmp::max;
 use std::env;
 use std::ops::{Mul, Sub};
 use std::process;
 
+struct Bn {
+    value: BigRational,
+    index: i32
+}
 
 struct Context {
-    n: usize,
     bigone_const: BigInt,
-    bigrat_const: BigRational,
     a: Vec<BigRational>,
-    index: i32    // For iterator
+    index: i32              // Counter for iterator implementation
 }
 
 impl Context {
-    pub fn new(up_to: usize) -> Context {
+    pub fn new() -> Context {
         let bigone = 1.to_bigint().unwrap();
-        let bigrat = BigRational::new(bigone.clone(), bigone.clone());
-        let a_vec: Vec<BigRational> = vec![bigrat.clone(); up_to + 1];
+        let a_vec: Vec<BigRational> = vec![];
         Context {
-            n: up_to,
             bigone_const: bigone,
-            bigrat_const: bigrat,
             a: a_vec,
             index: -1
         }
+    }
+}
+
+impl Iterator for Context {
+    type Item = Bn;
+
+    fn next(&mut self) -> Option<Bn> {
+        self.index += 1;
+        Some(Bn { value: bernoulli(self.index as usize, self), index: self.index })
     }
 }
 
@@ -54,33 +66,22 @@ fn main() {
         }
     }
 
-    let mut context = Context::new(up_to);
+    let context = Context::new();
+    // Collect the solutions by using the Context iterator
+    // (this is not as fast as calling the optimized function directly).
+    let res = context.take(up_to + 1).collect::<Vec<_>>();
+    let width = res.iter().fold(0, |a, r| max(a, r.value.numer().to_string().len()));
 
-    let mut res: Vec<(usize, BigRational)> = vec![];
-    let mut widths: Vec<u32> = vec![];
-
-    //for n in 0..up_to + 1 {
-    //    let b = bernoulli(n, &mut context);
-    //println!("got {} for {}", b, n);
-    let mut res = context.take(up_to + 1).collect::<Vec<_>>();
-    //let x = context.take(up_to + 1).count();
-    /*for r in context.take(up_to + 1) {
-        res.push(r.clone());
-        widths.push(r.1.numer().to_string().len());
-    }*/
-
-    //let width = widths.iter().max().unwrap();
-    let width = 10;
-    for r in res.iter().filter(|r| r.0 % 2 == 0) {
-        println!("B({:>2}) = {:>2$} / {denom}", r.0, r.1.numer(), width, denom = r.1.denom());
+    for r in res.iter().filter(|r| r.index % 2 == 0) {
+        println!("B({:>2}) = {:>2$} / {denom}", r.index, r.value.numer(), width,
+            denom = r.value.denom());
     }
 }
 
 // Implementation with no reused calculations.
-fn bernoulli_naive(n: usize, c: &mut Context) -> BigRational {
-
+fn _bernoulli_naive(n: usize, c: &mut Context) -> BigRational {
     for m in 0..n + 1 {
-        c.a[m] = BigRational::new(c.bigone_const.clone(), (m + 1).to_bigint().unwrap());
+        c.a.push(BigRational::new(c.bigone_const.clone(), (m + 1).to_bigint().unwrap()));
         for j in (1..m + 1).rev() {
             c.a[j - 1] = (c.a[j - 1].clone().sub(c.a[j].clone())).mul(
                 BigRational::new(j.to_bigint().unwrap(), c.bigone_const.clone())
@@ -91,11 +92,10 @@ fn bernoulli_naive(n: usize, c: &mut Context) -> BigRational {
 }
 
 // Implementation with reused calculations (does not require sequential calls).
-// This is ~100x faster.
 fn bernoulli(n: usize, c: &mut Context) -> BigRational {
-    for i in 1..n + 1 {
-        if c.a[i].eq(&c.bigrat_const) {
-            c.a[i] = BigRational::new(c.bigone_const.clone(), (i + 1).to_bigint().unwrap());
+    for i in 0..n + 1 {
+        if i >= c.a.len() {
+            c.a.push(BigRational::new(c.bigone_const.clone(), (i + 1).to_bigint().unwrap()));
             for j in (1..i + 1).rev() {
                 c.a[j - 1] = (c.a[j - 1].clone().sub(c.a[j].clone())).mul(
                     BigRational::new(j.to_bigint().unwrap(), c.bigone_const.clone())
@@ -106,33 +106,18 @@ fn bernoulli(n: usize, c: &mut Context) -> BigRational {
     c.a[0].reduced()
 }
 
-// Iterator implementation.
-impl Iterator for Context {
-    type Item = (usize, BigRational);
-
-    fn next(&mut self) -> Option<(usize, BigRational)> {
-        self.index += 1;
-        Some((self.index as usize, self.bigrat_const.clone()))
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
-    use super::{Context, bernoulli, bernoulli_naive};
+    use super::{Bn, Context, bernoulli, _bernoulli_naive};
     use num::rational::{BigRational};
     use std::str::FromStr;
     use test::Bencher;
 
     #[test]
     fn test_bernoulli_naive() {
-        let mut context = Context::new(60);
-    }
-
-    #[test]
-    fn test_bernoulli_naive() {
-        let mut context = Context::new(60);
-        assert_eq!(bernoulli_naive(60, &mut context), BigRational::new(
+        let mut context = Context::new();
+        assert_eq!(_bernoulli_naive(60, &mut context), BigRational::new(
                 FromStr::from_str("-1215233140483755572040304994079820246041491").unwrap(),
                 FromStr::from_str("56786730").unwrap()
             )
@@ -141,8 +126,19 @@ mod tests {
 
     #[test]
     fn test_bernoulli() {
-        let mut context = Context::new(60);
+        let mut context = Context::new();
         assert_eq!(bernoulli(60, &mut context), BigRational::new(
+                FromStr::from_str("-1215233140483755572040304994079820246041491").unwrap(),
+                FromStr::from_str("56786730").unwrap()
+            )
+        );
+    }
+
+    #[test]
+    fn test_bernoulli_iter() {
+        let context = Context::new();
+        let res = context.take(60 + 1).collect::<Vec<_>>();
+        assert_eq!(res.last().unwrap().value, BigRational::new(
                 FromStr::from_str("-1215233140483755572040304994079820246041491").unwrap(),
                 FromStr::from_str("56786730").unwrap()
             )
@@ -151,25 +147,33 @@ mod tests {
 
     #[bench]
     fn bench_bernoulli_naive(b: &mut Bencher) {
-        let mut context = Context::new(30);
+        let mut context = Context::new();
         b.iter(|| {
-            let mut res: Vec<(usize, BigRational)> = vec![];
+            let mut res: Vec<Bn> = vec![];
             for n in 0..30 + 1 {
-                let b = bernoulli_naive(n, &mut context);
-                res.push((n, b.clone()));
+                let b = _bernoulli_naive(n, &mut context);
+                res.push(Bn { value:b.clone(), index: n as i32});
             }
         });
     }
 
     #[bench]
     fn bench_bernoulli(b: &mut Bencher) {
-        let mut context = Context::new(30);
+        let mut context = Context::new();
         b.iter(|| {
-            let mut res: Vec<(usize, BigRational)> = vec![];
+            let mut res: Vec<Bn> = vec![];
             for n in 0..30 + 1 {
                 let b = bernoulli(n, &mut context);
-                res.push((n, b.clone()));
+                res.push(Bn { value:b.clone(), index: n as i32});
             }
+        });
+    }
+
+    #[bench]
+    fn bench_bernoulli_iter(b: &mut Bencher) {
+        b.iter(|| {
+            let context = Context::new();
+            let _res = context.take(30 + 1).collect::<Vec<_>>();
         });
     }
 }
