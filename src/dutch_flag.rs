ab34d01293ea77c4b9598cd8cb3281b262227df0
borissmidt <smidtboris1@gmail.com>
2016-03-31 18:43:36 +0200

    i have implemented the dutch national flag task where you need to -generate a rondom array -check if it sorted -sort the array -print the array in words ok there is a posibility to write a sort ourselfs but is this really required when rust has it out of the box?
    forgot to uncomment the eventual lib
    
    forgot to uncomment the eventual lib

---
+++ src/dutch_flag.rs
@@ -0,0 +1,49 @@
+// http://rosettacode.org/wiki/Dutch_national_flag_problem
+
+extern crate rand;
+use rand::Rng;
+
+// if you use an enum you will have to implement a lot of traits for it before you can
+// iterate and sort it my proposal is to just use u8 and image a C like enum thus
+// 0 = red
+// 1 = white
+// 2 = blue
+//
+// look up table
+const COLOR: &'static [&'static str] = &["Red", "White", "Blue"];
+
+fn check_sorted(color_array: &[u8]) -> bool {
+    let mut test_color = 0;
+    for color in color_array {
+        if *color < test_color {
+            return false;
+        }
+        test_color = *color;
+    }
+    return true;
+}
+
+fn color_to_string(color_array: &[u8]) -> String {
+    color_array.iter().fold("".to_string(), |string, &x| string + COLOR[x as usize])
+}
+
+fn main() {
+
+    let mut rng = rand::thread_rng();
+    let mut color_array = [0u8; 20];
+
+    for color in color_array.iter_mut() {
+        *color = rng.gen_range(0, 3);
+    }
+
+    if check_sorted(&color_array) {
+        println!("oops i generated a sorted array {}",
+                 color_to_string(&color_array));
+    } else {
+        println!("random flag {}", color_to_string(&color_array));
+    }
+
+    color_array.sort();
+    println!("a dutch flag {}", color_to_string(&color_array));
+
+}
