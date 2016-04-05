3ea11a06d1a6ec360a5c224a835b78a7d58a35c9
allenbui94 <allenbui94@gmail.com>
2016-04-05 07:59:58 -0700

    add solution to jaro_distance

---
+++ src/jaro_distance.rs
@@ -0,0 +1,103 @@
+// http://rosettacode.org/wiki/Jaro_distance
+use std::cmp;
+
+fn jaro(str1: &str, str2: &str) -> f64 {
+    // lengths of both strings
+    let str1_len: usize = str1.len();
+    let str2_len: usize = str2.len();
+
+    // if both strings are empty return 1
+    // if only one of the strings is empty return 0
+    if str1_len == 0 {
+        if str2_len == 0 {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    }
+
+    // max distance between two chars to be considered matching
+    let match_distance: isize = cmp::max(str1_len, str2_len) as isize / 2 - 1;
+
+    // mutable vectors of bools that signify if that char in the matching string has a match
+    let mut str1_matches: Vec<bool> = vec![false; str1_len];
+    let mut str2_matches: Vec<bool> = vec![false; str2_len];
+
+    // number of matches and transpositions
+    let mut matches: f64 = 0.0;
+    let mut transpositions: f64 = 0.0;
+
+    // find the matches
+    for i in 0_usize..str1_len {
+        // cast new variable i_isize for clarity
+        let i_isize = i as isize;
+        // start and end take into account the match distance
+        let start: usize = cmp::max(0, i_isize - match_distance) as usize;
+        let end: usize = cmp::min(i_isize + match_distance + 1, str2_len as isize) as usize;
+
+        for k in start..end {
+            // if str2 already has a match continue
+            if str2_matches[k] {
+                continue;
+            }
+            // if str1 at i and str2 at k are not equal
+            if str1.chars().nth(i).unwrap() != str2.chars().nth(k).unwrap() {
+                continue;
+            }
+            // otherwise assume there is a match
+            str1_matches[i] = true;
+            str2_matches[k] = true;
+            matches += 1.0;
+            break;
+        }
+    }
+
+    // if there are no matches return 0
+    if matches == 0.0 {
+        return 0.0;
+    }
+
+    // count transpositions
+    let mut k = 0;
+    for i in 0_usize..str1_len {
+        // if there are no matches in str1 continue
+        if !str1_matches[i] {
+            continue;
+        }
+        // while there is no match in str2 increment k
+        while !str2_matches[k] {
+            k += 1;
+        }
+        // increment transpositions
+        if str1.chars().nth(i).unwrap() != str2.chars().nth(k).unwrap() {
+            transpositions += 1.0;
+        }
+        k += 1;
+    }
+
+    // deallocate variables no longer used
+    drop(k);
+    drop(str1_matches);
+    drop(str2_matches);
+    drop(match_distance);
+
+    // divide the number of transpositions by two as per the algorithm specs
+    transpositions /= 2.0;
+
+    // return the Jaro distance
+    ((matches / str1_len as f64) + (matches / str2_len as f64) +
+     ((matches - transpositions) / matches)) / 3.0
+}
+
+fn main() {
+    println!("{}", jaro("MARTHA", "MARHTA"));
+    println!("{}", jaro("DIXON", "DICKSONX"));
+    println!("{}", jaro("JELLYFISH", "SMELLYFISH"));
+}
+
+#[test]
+fn test_jaro() {
+    assert_eq!(jaro("MARTHA", "MARHTA"), 0.9444444444444445);
+    assert_eq!(jaro("DIXON", "DICKSONX"), 0.7666666666666666);
+    assert_eq!(jaro("JELLYFISH", "SMELLYFISH"), 0.8962962962962964);
+}
