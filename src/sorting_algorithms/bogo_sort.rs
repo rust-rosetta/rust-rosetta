d1eb959f1044dd9dbee7f2b930c2760aa799d60a
Allen Bui <allenbui94@gmail.com>
2016-04-05 19:33:39 -0700

    format and refactor bogo_sort

--- src/sorting_algorithms/bogo_sort.rs
+++ src/sorting_algorithms/bogo_sort.rs
@@ -4,12 +4,13 @@ use rand::{thread_rng, Rng};
 
 fn bogo_sort<T: Ord>(mut v: &mut [T]) {
     let mut rng = thread_rng();
-    while !check_sort(v) {
+    while !is_sorted(v) {
         rng.shuffle(&mut v);
     }
 }
-// helper function that checks for ascending order (might be renamed)
-fn check_sort<T: Ord>(v: &[T]) -> bool {
+
+// helper function that checks for ascending order
+fn is_sorted<T: Ord>(v: &[T]) -> bool {
     if v.len() > 1 {
         for i in 0..(v.len() - 1) {
             if v[i] > v[i + 1] {
@@ -19,46 +20,50 @@ fn check_sort<T: Ord>(v: &[T]) -> bool {
     }
     true
 }
+
 fn main() {
     let mut numbers = [4i32, 65, 2, -31, 0, 99, 2, 83, 782, 1];
     println!("Before: {:?}", numbers);
     bogo_sort(&mut numbers);
     println!("After: {:?}", numbers);
 }
+
 #[cfg(test)]
 mod tests {
     use super::bogo_sort;
+
     fn check_sort<T: Ord>(v: &[T]) {
-        if v.len() > 1 {
-            for i in 0..(v.len() - 1) {
-                assert!(v[i] <= v[i + 1]);
-            }
-        }
+        assert!(super::is_sorted(v));
     }
+
     #[test]
     fn test_rosetta_vector() {
         let numbers = &mut [4i32, 65, 2, -31, 0, 99, 2, 83, 782, 1];
         bogo_sort(numbers);
         check_sort(numbers);
     }
+
     #[test]
     fn test_empty_vector() {
         let mut numbers: Vec<i32> = Vec::new();
         bogo_sort(&mut numbers[..]);
         check_sort(&mut numbers[..]);
     }
+
     #[test]
     fn test_one_element_vector() {
         let numbers = &mut [0i32];
         bogo_sort(numbers);
         check_sort(numbers);
     }
+
     #[test]
     fn test_repeat_vector() {
         let numbers = &mut [1i32, 1, 1, 1, 1];
         bogo_sort(numbers);
         check_sort(numbers);
     }
+
     #[test]
     fn test_string_vector() {
         let strings = &mut ["beach", "hotel", "airplane", "car", "house", "art"];
