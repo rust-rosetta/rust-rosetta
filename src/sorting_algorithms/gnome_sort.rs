406a6379fdd9cf643f127d212d4b6b19762ebe3d
allenbui94 <allenbui94@gmail.com>
2016-04-08 08:29:40 -0700

    add solution to gnome sort

---
+++ src/sorting_algorithms/gnome_sort.rs
@@ -0,0 +1,68 @@
+// http://rosettacode.org/wiki/Sorting_algorithms/Gnome_sort
+
+fn gnome_sort<T: PartialOrd>(v: &mut [T]) {
+    let len = v.len();
+    let mut i: usize = 1;
+    let mut j: usize = 2;
+    while i < len {
+        if v[i - 1] <= v[i] {
+            i = j;
+            j = j + 1;
+        } else {
+            v.swap(i - 1, i);
+            i = i - 1;
+            if i == 0 {
+                i = j;
+                j = j + 1;
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut numbers = [4i32, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+    println!("Before: {:?}", numbers);
+    gnome_sort(&mut numbers);
+    println!("After: {:?}", numbers);
+}
+
+#[cfg(test)]
+mod tests {
+    fn check_sort<T: PartialOrd>(v: &mut [T]) {
+        super::gnome_sort(v);
+
+        for i in 1..v.len() {
+            assert!(v[i - 1] <= v[i]);
+        }
+    }
+
+    #[test]
+    fn rosetta_vector() {
+        let numbers = &mut [4i32, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn empty_vector() {
+        let mut numbers: &mut [i32] = &mut [];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn one_element_vector() {
+        let numbers = &mut [0i32];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn repeat_vector() {
+        let numbers = &mut [1i32, 1, 1, 1, 1];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn already_sorted_vector() {
+        let numbers = &mut [-1i32, 0, 3, 6, 99];
+        check_sort(numbers);
+    }
+}
