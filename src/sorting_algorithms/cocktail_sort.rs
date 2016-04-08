ba05dfd1dcb2cf0ca3387b09c8e479eb3cc0e753
allenbui94 <allenbui94@gmail.com>
2016-04-08 07:47:27 -0700

    resolve merge conflict in cargo.toml

---
+++ src/sorting_algorithms/cocktail_sort.rs
@@ -0,0 +1,82 @@
+// http://rosettacode.org/wiki/Sorting_algorithms/Cocktail_sort
+
+// Progress through the slice and 'bubble' elements up and down until they are in order.
+fn cocktail_sort<T: PartialOrd>(v: &mut [T]) {
+    (1..v.len() + 1).rev().all(|length| {
+        let mut swapped: bool = false;
+        // bubble up
+        for index in 0..length - 1 {
+            if v[index] > v[index + 1] {
+                swapped = true;
+                v.swap(index, index + 1);
+            }
+        }
+        // break if no swap occured before bubbling down
+        if !swapped {
+            return false;
+        }
+        // bubble down
+        for index in (0..length - 1).rev() {
+            if v[index] > v[index + 1] {
+                swapped = true;
+                v.swap(index, index + 1);
+            }
+        }
+        // Continue to iterate if any swapping took place
+        swapped
+    });
+}
+
+fn main() {
+    let mut numbers = [4i32, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+    println!("Before: {:?}", numbers);
+    cocktail_sort(&mut numbers);
+    println!("After: {:?}", numbers);
+}
+
+#[cfg(test)]
+mod tests {
+    fn check_sort<T: PartialOrd>(v: &mut [T]) {
+        super::cocktail_sort(v);
+
+        for i in 1..v.len() {
+            assert!(v[i - 1] <= v[i]);
+        }
+    }
+
+    #[test]
+    fn rosetta_vector() {
+        let numbers = &mut [4i32, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn empty_vector() {
+        let mut numbers: &mut [i32] = &mut [];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn one_element_vector() {
+        let numbers = &mut [0i32];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn repeat_vector() {
+        let numbers = &mut [1i32, 1, 1, 1, 1];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn turtles_vector() {
+        let numbers = &mut [20i32, 10, 0, -1, -5];
+        check_sort(numbers);
+    }
+
+    #[test]
+    fn already_sorted_vector() {
+        let numbers = &mut [-1i32, 0, 3, 6, 99];
+        check_sort(numbers);
+    }
+}
