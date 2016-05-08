2787b1f434bad9f6742b5fbc835a7709d9cf3fa6
Andy Russell <arussell123@gmail.com>
2016-05-08 16:53:56 -0400

    fix single-character variable names

--- src/md5/implementation.rs
+++ src/md5/implementation.rs
@@ -62,6 +62,7 @@ fn to_bytes(val: u64) -> [u8; 8] {
     tmp
 }
 
+#[cfg_attr(feature="clippy", allow(many_single_char_names))]
 fn md5(initial_msg: &[u8]) -> MD5 {
     let initial_len = initial_msg.len() as u64;
 
