1825a92ebd4bf087f91403483dfbfd7a83a037ba
Boris Egorov <egorov@linux.com>
2016-05-22 00:28:58 +0600

    Basic statistics: add tests

--- src/statistics_basic.rs
+++ src/statistics_basic.rs
@@ -4,7 +4,7 @@ extern crate rand;
 
 use rand::distributions::{IndependentSample, Range};
 
-fn mean(data: &[f32]) -> Option<f32> {
+pub fn mean(data: &[f32]) -> Option<f32> {
     if data.is_empty() {
         None
     } else {
@@ -13,7 +13,7 @@ fn mean(data: &[f32]) -> Option<f32> {
     }
 }
 
-fn variance(data: &[f32]) -> Option<f32> {
+pub fn variance(data: &[f32]) -> Option<f32> {
     if data.is_empty() {
         None
     } else {
@@ -26,7 +26,7 @@ fn variance(data: &[f32]) -> Option<f32> {
     }
 }
 
-fn standard_deviation(data: &[f32]) -> Option<f32> {
+pub fn standard_deviation(data: &[f32]) -> Option<f32> {
     if data.is_empty() {
         None
     } else {
@@ -67,5 +67,43 @@ fn main() {
         println!("Variance:           {:?}", variance(&data));
         println!("Standard deviation: {:?}", standard_deviation(&data));
         print_histogram(40, &data);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::f32;
+    use super::{mean, variance, standard_deviation};
+
+    fn approx(statistics: Option<f32>, value: f32) -> bool {
+        (statistics.unwrap() - value).abs() <= f32::EPSILON
+    }
+
+    #[test]
+    fn test_mean() {
+        let empty: Vec<f32> = vec![];
+        assert_eq!(mean(&empty), None);
+        assert!(approx(mean(&[1.0]), 1.0));
+        assert!(approx(mean(&[1.0, 3.0]), 2.0));
+        assert!(approx(mean(&[1.0, 2.0, 3.0]), 2.0));
+    }
+
+    #[test]
+    fn test_variance() {
+        let empty: Vec<f32> = vec![];
+        assert_eq!(variance(&empty), None);
+        assert!(approx(variance(&[0.0]), 0.0));
+        assert!(approx(variance(&[1.0, 1.0, 1.0]), 0.0));
+        assert!(approx(variance(&[1.0, 2.0, 3.0]), 2.0 / 3.0));
+    }
+
+    #[test]
+    fn test_standard_deviation() {
+        let empty: Vec<f32> = vec![];
+        assert_eq!(standard_deviation(&empty), None);
+        assert!(approx(standard_deviation(&[0.0]), 0.0));
+        assert!(approx(standard_deviation(&[1.0, 1.0, 1.0]), 0.0));
+        assert!(approx(standard_deviation(&[1.0, 2.0, 3.0]),
+                       (2.0f32 / 3.0f32).sqrt()));
     }
 }
