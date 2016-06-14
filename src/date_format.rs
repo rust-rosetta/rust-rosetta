b02f007a6aa8b2f2d47040328c0057d58bf2942b
orhanbalci <orhanbalci@gmail.com>
2016-06-14 14:57:02 +0300

    date format solution added

---
+++ src/date_format.rs
@@ -0,0 +1,9 @@
+// http://rosettacode.org/wiki/Date_format
+
+extern crate chrono;
+use chrono::*;
+fn main(){
+    let now : DateTime<UTC> = UTC::now();
+    println!("{}", now.format("%Y-%m-%d").to_string());
+    println!("{}", now.format("%A, %B %d, %Y").to_string());
+}
