39d9077f716b6760f3af30f9797ae05b628c9659
Andy Russell <arussell123@gmail.com>
2016-07-07 18:41:24 -0400

    remove source file coverage test
    This is handled by cargo itself now, as it will refuse to compile a crate that
    is inside a workspace but not in the workspace members.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 //! Library that contains utility functions for tests.
-//!
-//! It also contains a test module, which checks if all source files are covered by `Cargo.toml`
 
 extern crate hyper;
 extern crate regex;
@@ -87,11 +85,6 @@ fn main() {}
 
 #[cfg(test)]
 mod tests {
-    use regex::Regex;
-    use std::collections::HashSet;
-    use std::io::{BufReader, BufRead};
-    use std::fs::{self, File};
-    use std::path::Path;
 
     #[test]
     fn check_sorted() {
@@ -106,67 +99,5 @@ mod tests {
         let unsorted = vec![1, 3, 2];
 
         super::check_sorted(&unsorted);
-    }
-
-    /// A test to check if all source files are covered by `Cargo.toml`
-    #[test]
-    fn check_sources_covered() {
-        let sources = get_source_files();
-        let bins = get_toml_paths();
-        let not_covered = get_not_covered(&sources, &bins);
-
-        if !not_covered.is_empty() {
-            println!("Error, the following source files are not covered by Cargo.toml:");
-
-            for source in &not_covered {
-                println!("{}", source);
-            }
-
-            panic!("Please add the previous source files to Cargo.toml");
-        }
-    }
-
-    /// Returns the names of the source files in the `src` directory
-    fn get_source_files() -> HashSet<String> {
-        let paths = fs::read_dir("./src").unwrap();
-        paths.map(|p| {
-                p.unwrap()
-                    .path()
-                    .file_name()
-                    .unwrap()
-                    .to_os_string()
-                    .into_string()
-                    .unwrap()
-            })
-            .filter(|s| s[..].ends_with(".rs"))
-            .collect()
-    }
-
-    /// Returns the paths of the source files referenced in Cargo.toml
-    fn get_toml_paths() -> HashSet<String> {
-        let c_toml = File::open("./Cargo.toml").unwrap();
-        let reader = BufReader::new(c_toml);
-        let regex = Regex::new("path = \"(.*)\"").unwrap();
-        reader.lines()
-            .filter_map(|l| {
-                let l = l.unwrap();
-                regex.captures(&l).map(|c| {
-                    c.at(1)
-                        .map(|s| Path::new(s))
-                        .unwrap()
-                        .file_name()
-                        .unwrap()
-                        .to_string_lossy()
-                        .into_owned()
-                })
-            })
-            .collect()
-    }
-
-    /// Returns the filenames of the source files which are not covered by Cargo.toml
-    fn get_not_covered<'a>(sources: &'a HashSet<String>,
-                           paths: &'a HashSet<String>)
-                           -> HashSet<&'a String> {
-        sources.difference(paths).collect()
     }
 }
