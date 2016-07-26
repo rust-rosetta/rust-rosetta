f7962324c9edbfa3ce01da80819fdebf09404e14
Andy Russell <arussell123@gmail.com>
2016-07-26 13:01:17 -0400

    move "Include a file" into own crate

--- tasks/include-a-file/src/main.rs
+++ tasks/include-a-file/src/main.rs
@@ -1,8 +1,9 @@
-// http://rosettacode.org/wiki/Include_a_file
-
-// The compiler will include either a `test.rs` or `test/mod.rs` (if the first one doesn't exist)
-// file.
+// The compiler will import either `hello_world.rs` or `hello_world/mod.rs` (if the first one
+// doesn't exist).
 mod hello_world;
+
+// You can import public functions, structs, etc. from a module with the `use` statement.
+use hello_world::hello_world;
 
 // Additionally, third-party libraries (called `crates` in Rust) can be declared thusly:
 extern crate url;
@@ -10,6 +11,9 @@ extern crate url;
 use url::Url;
 
 fn main() {
+    // Call an imported function.
+    hello_world();
+
     // Use a struct included from an external crate.
     println!("{:?}", Url::parse("http://rosettacode.org").unwrap());
 
