955fbbf399643d6a645f9e5a4069ee9ab293bb0b
Andy Russell <arussell123@gmail.com>
2016-08-01 18:51:30 -0400

    move "Align columns" into own crate

--- tasks/align-columns/src/main.rs
+++ tasks/align-columns/src/main.rs
@@ -1,5 +1,3 @@
-// http://rosettacode.org/wiki/Align_columns
-
 const TEST_STR: &'static str = r"Given$a$text$file$of$many$lines,$where$fields$within$a$line$
 are$delineated$by$a$single$'dollar'$character,$write$a$program
 that$aligns$each$column$of$fields$by$ensuring$that$words$in$each$
