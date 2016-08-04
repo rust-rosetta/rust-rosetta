e95fe19cdeba3ccf474749f12ca5bf7f6b8e62eb
Andy Russell <arussell123@gmail.com>
2016-08-04 19:01:38 -0400

    move "Sorting algorithms/Shell sort" to own crate

--- tasks/sorting-algorithms/shell-sort/src/main.rs
+++ tasks/sorting-algorithms/shell-sort/src/main.rs
@@ -1,7 +1,6 @@
-// http://rosettacode.org/wiki/Sorting_algorithms/Shell_sort
-
+#[cfg(test)]
 #[macro_use]
-extern crate rust_rosetta;
+extern crate meta;
 
 #[cfg_attr(feature="clippy", allow(needless_range_loop))]
 fn shell_sort<T: Ord + Copy>(v: &mut [T]) {
