36282920c7e6f5fcdca0a40851f9b61a1c2acd58
Andy Russell <arussell123@gmail.com>
2016-08-04 18:19:50 -0400

    move "Sorting algorithms/Bubble sort" to own crate

--- tasks/sorting-algorithms/bubble-sort/src/main.rs
+++ tasks/sorting-algorithms/bubble-sort/src/main.rs
@@ -1,7 +1,6 @@
-// http://rosettacode.org/wiki/Sorting_algorithms/Bubble_sort
-
+#[cfg(test)]
 #[macro_use]
-extern crate rust_rosetta;
+extern crate meta;
 
 /// Progress through the slice and 'bubble' elements up until they are in order.
 #[cfg_attr(feature = "clippy", allow(needless_range_loop))]
