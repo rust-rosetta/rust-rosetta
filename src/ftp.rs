09a0c9c5e8e894ab7bb9fd3bd58d66d4beed46e8
Corentin Henry <corentinhenry@gmail.com>
2016-08-17 15:06:52 -0700

    ftp: implement ftp task
    http://rosettacode.org/wiki/FTP

---
+++ src/ftp.rs
@@ -0,0 +1,36 @@
+extern crate ftp;
+
+use ftp::FtpStream;
+use ftp::types::FileType;
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    // connect to the server
+    let mut ftp = FtpStream::connect("kernel.org:21").unwrap();
+    ftp.login("anonymous", "").unwrap();
+
+    // change working directory
+    ftp.cwd("/pub/linux/kernel").unwrap();
+
+    // list files in the current directory
+    match ftp.list(None) {
+        Ok(output) => println!("{}", output.join("")),
+        Err(err) => panic!(err)
+    }
+
+    // download a file a write it on the disk
+    ftp.transfer_type(FileType::Binary).unwrap();
+    ftp.retr("README", |stream| {
+        let mut file = File::create("README").unwrap();
+        let mut buf = [0; 2048];
+        loop {
+            match stream.read(&mut buf) {
+                Ok(0) => break,
+                Ok(n) => file.write_all(&buf[0..n]).unwrap(),
+                Err(err) => panic!(err)
+            };
+        }
+        Ok(())
+    }).unwrap();
+}
