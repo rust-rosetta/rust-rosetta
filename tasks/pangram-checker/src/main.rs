b583c998cd302f7bae8609ccd84f2688efadc91b
Ky Waegel <kwaegel@users.noreply.github.com>
2016-10-13 22:36:13 -0700

    Added HashSet implementation
    * Also cleaned up testing code.

--- tasks/pangram-checker/src/main.rs
+++ tasks/pangram-checker/src/main.rs
@@ -3,6 +3,8 @@
 
 extern crate test;
 
+use std::collections::HashSet;
+
 pub fn is_pangram_via_bitmask(s: &str) -> bool {
 
     // Create a mask of set bits and convert to false as we find characters.
@@ -18,11 +20,25 @@ pub fn is_pangram_via_bitmask(s: &str) -> bool {
     mask == 0
 }
 
+pub fn is_pangram_via_hashset(s: &str) -> bool {
+
+    // Insert lowercase letters into a HashSet, then check if we have at least 26.
+    let letters = s.chars()
+        .flat_map(|chr| chr.to_lowercase())
+        .filter(|&chr| chr >= 'a' && chr <= 'z')
+        .fold(HashSet::new(), |mut letters, chr| {
+            letters.insert(chr);
+            letters
+        });
+
+    letters.len() == 26
+}
+
 pub fn is_pangram_via_sort(s: &str) -> bool {
 
-    // Copy chars into a vector, sort and remove duplicates
-    let mut chars: Vec<char> = s.to_lowercase()
-        .chars()
+    // Copy chars into a vector, convert to lowercase, sort, and remove duplicates.
+    let mut chars: Vec<char> = s.chars()
+        .flat_map(|chr| chr.to_lowercase())
         .filter(|&chr| chr >= 'a' && chr <= 'z')
         .collect();
 
@@ -38,13 +54,18 @@ fn main() {
                     "The quick white cat jumps over the lazy dog"];
 
     for &text in examples.iter() {
-        let is_pangram = is_pangram_via_sort(text);
-        println!("Is \"{}\" a pangram via sort? - {}", text, is_pangram);
-    }
+        let is_pangram_sort = is_pangram_via_sort(text);
+        println!("Is \"{}\" a pangram via sort? - {}", text, is_pangram_sort);
 
-    for &text in examples.iter() {
-        let is_pangram = is_pangram_via_bitmask(text);
-        println!("Is \"{}\" a pangram via bitmask? - {}", text, is_pangram);
+        let is_pangram_bitmask = is_pangram_via_bitmask(text);
+        println!("Is \"{}\" a pangram via bitmask? - {}",
+                 text,
+                 is_pangram_bitmask);
+
+        let is_pangram_hashset = is_pangram_via_hashset(text);
+        println!("Is \"{}\" a pangram via bitmask? - {}",
+                 text,
+                 is_pangram_hashset);
     }
 }
 
@@ -55,30 +76,34 @@ mod tests {
 
     #[test]
     fn test_sort() {
-        let result = is_pangram_via_sort("The quick brown fox jumps over the lazy dog");
-        assert!(result);
-
-        let result2 = is_pangram_via_sort("The quick white cat jumps over the lazy dog");
-        assert!(!result2);
+        assert!(is_pangram_via_sort("The quick brown fox jumps over the lazy dog"));
+        assert!(!is_pangram_via_sort("The quick white cat jumps over the lazy dog"));
     }
 
     #[test]
     fn test_bitmask() {
-        let result1 = is_pangram_via_bitmask("The quick brown fox jumps over the lazy dog");
-        assert!(result1);
+        assert!(is_pangram_via_bitmask("The quick brown fox jumps over the lazy dog"));
+        assert!(!is_pangram_via_bitmask("The quick white cat jumps over the lazy dog"));
+    }
 
-        let result2 = is_pangram_via_bitmask("The quick white cat jumps over the lazy dog");
-        assert!(!result2);
+    #[test]
+    fn test_hashset() {
+        assert!(is_pangram_via_hashset("The quick brown fox jumps over the lazy dog"));
+        assert!(!is_pangram_via_hashset("The quick white cat jumps over the lazy dog"));
     }
 
     #[bench]
     fn sort_speed(b: &mut Bencher) {
-        b.iter(|| is_pangram_via_sort("The quick white cat jumps over the lazy dog"));
+        b.iter(|| is_pangram_via_sort("The quick brown fox jumps over the lazy dog"));
     }
 
     #[bench]
     fn bitmask_speed(b: &mut Bencher) {
-        b.iter(|| is_pangram_via_bitmask("The quick white cat jumps over the lazy dog"));
+        b.iter(|| is_pangram_via_bitmask("The quick brown fox jumps over the lazy dog"));
     }
 
+    #[bench]
+    fn hashset_speed(b: &mut Bencher) {
+        b.iter(|| is_pangram_via_hashset("The quick brown fox jumps over the lazy dog"));
+    }
 }
