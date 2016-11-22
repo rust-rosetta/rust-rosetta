0e0fc048bc9b4f4875627d152732ebed7fc99ff7
Andy Russell <arussell123@gmail.com>
2016-11-22 12:32:29 -0500

    move "Levenshtein distance" to own crate

--- tasks/levenshtein-distance/src/main.rs
+++ tasks/levenshtein-distance/src/main.rs
@@ -1,5 +1,3 @@
-// http://rosettacode.org/wiki/Levenshtein_distance
-
 #[cfg_attr(feature="clippy", allow(needless_range_loop))]
 fn levenshtein_distance(word1: &str, word2: &str) -> usize {
     let word1_length = word1.len() + 1;
