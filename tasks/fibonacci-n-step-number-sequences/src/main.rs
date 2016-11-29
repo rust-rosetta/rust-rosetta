86cdcbbdfe62a8e36c32f1f787e45a182f4e9628
Andy Russell <arussell123@gmail.com>
2016-11-29 14:39:15 -0500

    move "Fibonacci N-step number sequences" to crate

--- tasks/fibonacci-n-step-number-sequences/src/main.rs
+++ tasks/fibonacci-n-step-number-sequences/src/main.rs
@@ -1,5 +1,3 @@
-// http://rosettacode.org/wiki/Fibonacci_n-step_number_sequences
-
 /// state for producing generalized Fibonacci sequences
 struct GenFibonacci {
     /// current values being summed
