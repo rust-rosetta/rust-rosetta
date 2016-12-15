2e13bddf845f2077fff589baa80c0a28dc7b8609
Andy Russell <arussell123@gmail.com>
2016-12-15 14:55:53 -0500

    copy "Case-sensitivity of identifiers" from wiki

---
+++ tasks/case-sensitivity-of-identifiers/src/main.rs
@@ -0,0 +1,8 @@
+#![allow(non_snake_case)]
+
+fn main() {
+    let dog = "Benjamin";
+    let Dog = "Samba";
+    let DOG = "Bernie";
+    println!("The three dogs are named {}, {} and {}.", dog, Dog, DOG);
+}
