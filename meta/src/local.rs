bd9bf4d02d0d3a92372c8057c488744bd063ff4d
Andy Russell <arussell123@gmail.com>
2017-10-10 15:38:13 -0400

    upgrade to toml 0.4

--- meta/src/local.rs
+++ meta/src/local.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 
 use reqwest::Url;
-use toml::{self, Table, Value};
+use toml::Value;
 use walkdir::WalkDir;
 
 use TASK_URL_RE;
@@ -84,8 +84,8 @@ where
     let cargo_toml = parse_toml(path).unwrap();
 
     let members = {
-        let workspace_table = cargo_toml.get("workspace").unwrap();
-        match workspace_table.lookup("members") {
+        let workspace_table = &cargo_toml["workspace"];
+        match workspace_table.get("members") {
             Some(&Value::Array(ref members)) => {
                 members
                     .iter()
@@ -99,14 +99,14 @@ where
     members
 }
 
-fn parse_toml<P>(path: P) -> io::Result<Table>
+fn parse_toml<P>(path: P) -> io::Result<Value>
 where
     P: AsRef<Path>,
 {
     let mut toml_file = try!(File::open(path));
     let mut contents = String::new();
     try!(toml_file.read_to_string(&mut contents));
-    Ok(toml::Parser::new(&contents).parse().unwrap())
+    Ok(contents.parse().unwrap())
 }
 
 fn parse_task<P>(path: P) -> LocalTask
@@ -135,10 +135,13 @@ where
         .unwrap()
         .to_owned();
 
-    let member_toml = Value::Table(parse_toml(path.join("Cargo.toml")).unwrap());
+    let member_toml = parse_toml(path.join("Cargo.toml")).unwrap();
 
     let url = member_toml
-        .lookup("package.metadata.rosettacode.url")
+        .get("package")
+        .and_then(|p| p.get("metadata"))
+        .and_then(|m| m.get("rosettacode"))
+        .and_then(|m| m.get("url"))
         .ok_or(TaskParseError::MissingMetadata)
         .map(|metadata| metadata.as_str().unwrap())
         .and_then(|metadata| {
