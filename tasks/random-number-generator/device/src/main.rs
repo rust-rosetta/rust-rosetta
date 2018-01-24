93adec0d5d2887cb3f5d7d00e8f73663d8023d2d
TheIronBorn <TheIronBorn@users.noreply.github.com>
2018-01-24 21:16:33 +0000

    fix typo

--- tasks/random-number-generator/device/src/main.rs
+++ tasks/random-number-generator/device/src/main.rs
@@ -3,7 +3,7 @@ extern crate rand;
 use rand::{Rng, OsRng};
 
 fn main() {
-    // becuase `OsRng` opens files, it may fail
+    // because `OsRng` opens files, it may fail
     let mut rng = OsRng::new().unwrap();
 
     println!("{}", rng.gen::<u32>());
