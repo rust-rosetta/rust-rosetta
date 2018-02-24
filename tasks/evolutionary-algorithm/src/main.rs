29266f2a0535240400a147c4a0f7de66b2aee647
TheIronBorn <TheIronBorn@users.noreply.github.com>
2018-02-24 19:45:14 +0000

    uppercase, syntax, random_char update

--- tasks/evolutionary-algorithm/src/main.rs
+++ tasks/evolutionary-algorithm/src/main.rs
@@ -7,7 +7,7 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
-    let target = "methinks it is like a weasel"; // lowercase for readability
+    let target = "METHINKS IT IS LIKE A WEASEL";
     let copies = 100;
     let mutation_rate = 20; // 1/20 = 0.05 = 5%
 
@@ -68,8 +68,11 @@ fn evolve<R: Rng>(
 /// Computes the fitness of a sentence against a target string, returning the number of
 /// incorrect characters.
 fn fitness(target: &str, sentence: &str) -> usize {
-    let zip = sentence.chars().zip(target.chars());
-    zip.filter(|&(c1, c2)| c1 != c2).count()
+    sentence
+        .chars()
+        .zip(target.chars())
+        .filter(|&(c1, c2)| c1 != c2)
+        .count()
 }
 
 /// Mutation algorithm.
@@ -86,13 +89,13 @@ fn mutate<R: Rng>(rng: &mut R, sentence: &str, mutation_rate: u32) -> String {
     sentence.chars().map(maybe_mutate).collect()
 }
 
-/// Generates a random lowercase letter or space.
+/// Generates a random letter or space.
 fn random_char<R: Rng>(rng: &mut R) -> char {
-    // Returns a value in the range [a, z] + an extra slot for the space character.  '|' is the
-    // exclusive upper bound.  (The `u8` values could be cast to larger integers for a better
-    // chance of the RNG hitting the proper range).
-    match rng.gen_range(b'a', b'|') as char {
-        '{' => ' ', // the `char` after 'z'
-        c => c,
+    // Returns a value in the range [A, Z] + an extra slot for the space character.  (The `u8`
+    // values could be cast to larger integers for a better chance of the RNG hitting the proper
+    // range).
+    match rng.gen_range(b'A', b'Z' + 2) {
+        c if c == b'Z' + 1 => ' ', // the `char` after 'Z'
+        c => c as char,
     }
 }
