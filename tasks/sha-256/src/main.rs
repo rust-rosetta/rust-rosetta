3f3d3c1e51cb56ce0690dbf26df7742f7c2ae44a
TheIronBorn <TheIronBorn@users.noreply.github.com>
2018-02-28 03:59:33 +0000

    switch to ring crate

--- tasks/sha-256/src/main.rs
+++ tasks/sha-256/src/main.rs
@@ -1,22 +1,20 @@
 // http://rosettacode.org/wiki/SHA-256
 
-extern crate crypto;
+extern crate ring;
 
-use crypto::digest::Digest;
-use crypto::sha2::Sha256;
+use ring::digest::{digest, SHA256};
 
 fn main() {
     println!("{}", sha_256("Rosetta code"));
 }
 
 fn sha_256(input: &str) -> String {
-    let mut sh = Sha256::new();
-    sh.input_str(input);
-    sh.result_str()
+    let result = digest(&SHA256, input.as_bytes());
+    result.as_ref().iter().map(|b| format!("{:x}", b)).collect()
 }
 
 #[test]
 fn test_rosetta() {
-    assert_eq!(sha_256("Rosetta code"),
-               "764faf5c61ac315f1497f9dfa542713965b785e5cc2f707d6468d7d1124cdfcf".to_string());
+    let expected_hash = "764faf5c61ac315f1497f9dfa542713965b785e5cc2f707d6468d7d1124cdfcf";
+    assert_eq!(sha_256("Rosetta code"), expected_hash);
 }
