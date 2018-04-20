3b327472af346389734d0c9fad555a65987a58f7
Andy Russell <arussell123@gmail.com>
2018-04-20 16:47:56 -0400

    simplify FTP task
    Remove the use of `impl Trait` by using the `simple_retr` method instead
    of a function that returns a callback. Also, remove the tests because
    they just test the functionality of the FTP crate, not our example.
    They're also dependent on state outside of our control.

--- tasks/ftp/src/main.rs
+++ tasks/ftp/src/main.rs
@@ -1,27 +1,12 @@
-#![feature(conservative_impl_trait)]
-
 extern crate ftp;
 
-use ftp::types::{FileType, Result};
-use ftp::FtpStream;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io;
 
-fn write_file(filename: &str) -> impl Fn(&mut Read) -> Result<()> {
-    let filename = filename.to_string();
-    move |stream| {
-        let mut file = File::create(&filename).unwrap();
-        let mut buf = [0; 2048];
-        loop {
-            match stream.read(&mut buf) {
-                Ok(0) => break,
-                Ok(n) => file.write_all(&buf[0..n]).unwrap(),
-                Err(err) => panic!(err),
-            };
-        }
-        Ok(())
-    }
-}
+use ftp::types::FileType;
+use ftp::FtpStream;
+
+static FILENAME: &str = "README";
 
 fn main() {
     // connect to the server
@@ -32,69 +17,12 @@ fn main() {
     ftp.cwd("/pub/linux/kernel").unwrap();
 
     // list files in the current directory
-    match ftp.list(None) {
-        Ok(output) => println!("{}", output.join("")),
-        Err(err) => panic!(err),
-    }
+    let list = ftp.list(None).unwrap();
+    println!("{}", list.join(""));
 
     // download a file a write it on the disk
     ftp.transfer_type(FileType::Binary).unwrap();
-    ftp.retr("README", write_file("README")).unwrap();
-}
-
-#[cfg(test)]
-mod test {
-    use super::write_file;
-    use ftp::types::FileType;
-    use ftp::FtpStream;
-    use std::fs;
-
-    fn connect() -> FtpStream {
-        let mut ftp = FtpStream::connect("kernel.org:21").unwrap();
-        ftp.login("anonymous", "").unwrap();
-        return ftp;
-    }
-
-    #[ignore]
-    #[test]
-    fn test_cwd() {
-        let mut ftp = connect();
-        // make sure the current directory is /
-        assert_eq!(ftp.pwd().unwrap(), "/");
-        ftp.cwd("/pub/linux/kernel").unwrap();
-        assert_eq!(ftp.pwd().unwrap(), "/pub/linux/kernel");
-    }
-
-    #[ignore]
-    #[test]
-    fn test_list_dir() {
-        let mut ftp = connect();
-        assert_eq!(
-            ftp.list(Some("/")).unwrap().join(""),
-            "drwxr-xr-x    9 ftp      ftp          4096 Dec 01  2011 pub"
-        );
-    }
-
-    #[ignore]
-    #[test]
-    fn test_download_file() {
-        let filename = ".test_download_file";
-        let mut ftp = connect();
-        ftp.cwd("/pub/linux/kernel").unwrap();
-        // make sure the file does not already exist
-        match fs::metadata(filename) {
-            Ok(_) => fs::remove_file(filename).unwrap(),
-            Err(_) => {}
-        }
-        ftp.transfer_type(FileType::Binary).unwrap();
-        ftp.retr("README", write_file(filename)).unwrap();
-        match fs::metadata(filename) {
-            Ok(metadata) => {
-                assert_eq!(metadata.is_file(), true);
-                assert_eq!(metadata.len(), 12056);
-                fs::remove_file(filename).unwrap();
-            }
-            Err(_) => panic!("file not downloaded"),
-        }
-    }
+    let mut contents = ftp.simple_retr(FILENAME).unwrap();
+    let mut file = File::create(FILENAME).unwrap();
+    io::copy(&mut contents, &mut file).unwrap();
 }
