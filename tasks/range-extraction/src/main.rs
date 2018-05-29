d680357f34887fa88fad4d9de1c7be70ad831cda
jhwgh1968 <jhwgh1968@users.noreply.github.com>
2018-05-28 20:28:56 -0500

    range-extraction: prints correct output

--- tasks/range-extraction/src/main.rs
+++ tasks/range-extraction/src/main.rs
@@ -49,9 +49,12 @@ impl<'a, T> RangeFinder<'a, T> {
 }
 
 fn main() {
-    let n = [0, 1, 2, 3];
+    let n = &[
+        0, 1, 2, 4, 6, 7, 8, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29,
+        30, 31, 32, 33, 35, 36, 37, 38, 39,
+    ];
 
-    for (i, (lo, hi)) in RangeFinder::new(&n).enumerate() {
+    for (i, (lo, hi)) in RangeFinder::new(n).enumerate() {
         if i > 0 {
             print!(", ")
         }
