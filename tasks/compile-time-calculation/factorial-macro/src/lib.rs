29f4a41663bf547a84f839454636d11ef9ac7b9d
Andy Russell <arussell123@gmail.com>
2018-06-16 12:34:51 -0400

    rewrite factorial plugin as proc macro

---
+++ tasks/compile-time-calculation/factorial-macro/src/lib.rs
@@ -0,0 +1,22 @@
+#![feature(proc_macro)]
+
+extern crate proc_macro;
+#[macro_use]
+extern crate quote;
+extern crate syn;
+
+use proc_macro::TokenStream;
+use syn::{ExprLit, Lit};
+
+#[proc_macro]
+pub fn factorial(input: TokenStream) -> TokenStream {
+    match syn::parse(input) {
+        Ok(ExprLit {
+            lit: Lit::Int(lit), ..
+        }) => {
+            let result: u64 = (1..=lit.value()).product();
+            result.to_string().parse().unwrap()
+        }
+        _ => quote!(compile_error!("argument must be an integer literal")).into(),
+    }
+}
--- tasks/compile-time-calculation/src/main.rs
+++ tasks/compile-time-calculation/src/main.rs
@@ -1,9 +1,9 @@
-// syntax extension are not yet stable, so we need to opt-in
-// explicitly to the phase feature gate
-#![feature(plugin)]
-// we use this attribute to mark factorial_plugin as
-// a syntax extension. The plugin's code is in src/factorial_plugin
-#![plugin(factorial_plugin)]
+#![feature(proc_macro_non_items)]
+#![feature(use_extern_macros)]
+
+extern crate factorial_macro;
+
+use factorial_macro::factorial;
 
 fn main() {
     // we can invoke factorial_10! as a regular macro
