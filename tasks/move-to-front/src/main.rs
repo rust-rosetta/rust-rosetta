fc6b38ed84b2814531e9177bfcc4114d132f8c0d
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-02-27 11:44:29 +0100

    add test

--- tasks/move-to-front/src/main.rs
+++ tasks/move-to-front/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    dbg!(encode("broood"));
-    dbg!(encode("bananaaa"));
-    dbg!(encode("hiphophiphop"));
+    let examples = vec!["broood", "bananaaa", "hiphophiphop"];
+    for example in examples {
+        let encoded = encode(example);
+        println!("{} encodes to {:?}", example, encoded);
+    }
 }
 
 fn get_symbols() -> Vec<u8> {
@@ -20,4 +22,15 @@ fn encode(input: &str) -> Vec<usize> {
             (o, s)
         })
         .0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn correct_encode() {
+        let encoded = encode("broood");
+        assert_eq!(encoded, vec![1,17,15,0,0,5]);
+    }
 }
