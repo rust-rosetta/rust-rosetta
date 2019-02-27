09fcc70931e290d055f3cda3fb44c5c1d06a7d4d
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-02-27 11:38:05 +0100

    implement encode

--- tasks/move-to-front/src/main.rs
+++ tasks/move-to-front/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    dbg!(encode("broood"));
+    dbg!(encode("bananaaa"));
+    dbg!(encode("hiphophiphop"));
+}
+
+fn get_symbols() -> Vec<u8> {
+    (b'a'..b'z').collect()
+}
+
+fn encode(input: &str) -> Vec<usize> {
+    input
+        .as_bytes()
+        .iter()
+        .fold((Vec::new(), get_symbols()), |(mut o, mut s), x| {
+            let i = s.iter().position(|c| c == x).unwrap();
+            let c = s.remove(i);
+            s.insert(0, c);
+            o.push(i);
+            (o, s)
+        })
+        .0
 }
