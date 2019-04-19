af51649f24e8e6a35085aba6222870e1170df1d8
Polochon-street <polochonstreet@gmx.fr>
2019-04-19 19:02:27 +0200

    Added isin validation

---
+++ tasks/validate-isin/src/lib.rs
@@ -0,0 +1,40 @@
+struct Digits {
+    m: u64,
+}
+
+impl Iterator for Digits {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        match self.m {
+            0 => None,
+            n => {
+                let ret = n % 10;
+                self.m = n / 10;
+                Some(ret)
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum LuhnState {
+    Even,
+    Odd,
+}
+
+fn digits(n: u64) -> Digits {
+    Digits { m: n }
+}
+
+pub fn luhn_test(n: u64) -> bool {
+    let odd_even = [LuhnState::Odd, LuhnState::Even];
+    let numbers = digits(n).zip(odd_even.iter().cycle().cloned());
+    let sum = numbers.fold(0u64, |s, n| {
+        s + match n {
+            (n, LuhnState::Odd) => n,
+            (n, LuhnState::Even) => digits(n * 2).fold(0, |s, n| s + n),
+        }
+    });
+    sum % 10 == 0
+}
