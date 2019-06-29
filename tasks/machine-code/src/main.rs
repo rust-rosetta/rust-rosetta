ae4d8b6942243e7d5d22de1a6c8fcf030bdcd954
Polochon-street <polochonstreet@gmx.fr>
2019-06-29 23:09:49 +0200

    Added support for 64-bits arch

--- tasks/machine-code/src/main.rs
+++ tasks/machine-code/src/main.rs
@@ -1,25 +1,44 @@
 extern crate libc;
 
-use std::mem;
-use std::ptr::copy;
-
-const PAGE_SIZE: usize = 4096;
-
+#[cfg(all(
+    target_os = "linux",
+    any(target_pointer_width = "32", target_pointer_width = "64")
+))]
 fn main() {
-    let size = 9;
-    let bytes: [u8; 9] = [0x8b, 0x44, 0x24, 0x04, 0x03, 0x44, 0x24, 0x08, 0xc3];
+    use std::mem;
+    use std::ptr;
+
+    let page_size: usize = 4096;
+    let (bytes, size): (Vec<u8>, usize) = if cfg!(target_pointer_width = "32") {
+        (
+            vec![0x8b, 0x44, 0x24, 0x04, 0x03, 0x44, 0x24, 0x08, 0xc3],
+            9,
+        )
+    } else {
+        (vec![0x48, 0x89, 0xf8, 0x48, 0x01, 0xf0, 0xc3], 7)
+    };
     let f: fn(u8, u8) -> u8 = unsafe {
-        let mut page: *mut libc::c_void = mem::uninitialized();
-        libc::posix_memalign(&mut page, PAGE_SIZE, size);
+        let mut page: *mut libc::c_void = ptr::null_mut();
+        libc::posix_memalign(&mut page, page_size, size);
         libc::mprotect(
             page,
             size,
             libc::PROT_EXEC | libc::PROT_READ | libc::PROT_WRITE,
         );
-        let contents: *mut u8 = mem::transmute(page);
-        copy(bytes.as_ptr(), contents, 9);
+        let contents: *mut u8 = page as *mut u8;
+        ptr::copy(bytes.as_ptr(), contents, 9);
         mem::transmute(contents)
     };
 
-    println!("{}", f(7, 12));
+    let return_value = f(7, 12);
+    println!("Returned value: {}", return_value);
+    assert_eq!(return_value, 19);
+}
+
+#[cfg(any(
+    not(target_os = "linux"),
+    not(any(target_pointer_width = "32", target_pointer_width = "64"))
+))]
+fn main() {
+    println!("Not supported on this platform.");
 }
