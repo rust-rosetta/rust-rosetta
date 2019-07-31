bd2f730584861c5d68109a0f8ec22e8e7f790923
Eli Flanagan <eli@typedspace.com>
2019-07-31 19:45:31 -0400

    update comments, wire generation logic to CLI

--- tasks/password-generator/src/main.rs
+++ tasks/password-generator/src/main.rs
@@ -1,10 +1,12 @@
-// why use a library? Clap lets us quickly build a CLI and
+// declare our external dependencies
+// for parsing command line arguments
+extern crate clap;
+// for working with random values
+extern crate rand;
+// why use a library for this CLI? Clap lets us quickly build a CLI and
 // lets us focus the below code on password generation
 // not the intricacies building a CLI in rust.
 // Read more about clap here: https://clap.rs/
-// declare our external dependency for parsing command line arguments
-extern crate clap;
-extern crate rand;
 // bring the needed structures into scope so we
 // invoke them later in the program
 use clap::{App, Arg};
@@ -26,6 +28,7 @@ fn generate_password(length: usize) -> String {
         .collect();
     // create an iterator of required other characters
     let other_values = "!\"#$%&'()*+,-./:;<=>?@[]^_{|}~";
+    // create a random count of how many other characters to add
     let mut to_add = rng.gen_range(1, 10);
     loop {
         let special = other_values.chars().choose(&mut rng).unwrap();
@@ -44,7 +47,7 @@ fn main() {
     // clap provides powerful defaults so we don't have to
     // write all the logic here
     // For example, clap takes care of the help and version flags by default
-    App::new("password-generator")
+    let matches = App::new("password-generator")
         .version("0.1")
         .about("generate a password according to the rosetta code rules: http://rosettacode.org/wiki/Password_generator")
         // configure our first required argument
@@ -57,6 +60,7 @@ fn main() {
              // to avoid unexpected behavior
              .required(true)
              .index(1)
+             .takes_value(true)
             )
         // configure our second required argument
         .arg(Arg::with_name("COUNT")
@@ -64,8 +68,19 @@ fn main() {
              .default_value("1")
              .required(true)
              .index(2)
+             .takes_value(true)
             )
         .get_matches();
+
+    // It's safe to call unwrap because the value with either be what the user provides
+    // at runtime or our defaults configured above
+    let parsed_count: i32 = matches.value_of("COUNT").unwrap().parse().unwrap();
+    let parsed_length: usize = matches.value_of("LENGTH").unwrap().parse().unwrap();
+    // we don't need the index since we'll print to standard out
+    // in rust, underscores indicate unused variables
+    for _ in 0..parsed_count {
+        println!("{}", generate_password(parsed_length));
+    }
 }
 
 // declare a module for testing
