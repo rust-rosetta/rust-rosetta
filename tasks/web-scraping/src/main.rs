d8ef54f9aa79aa7ea56691530198677768879c50
Eli Flanagan <eli@typedspace.com>
2019-08-15 20:20:21 -0400

    test web scraping
    It appears cargo updates the Cargo.lock while running cargo test or
    cargo run with a new nightly.

--- tasks/web-scraping/src/main.rs
+++ tasks/web-scraping/src/main.rs
@@ -1,16 +1,8 @@
 use std::error::Error;
 /// Create a program that downloads the time from this URL: http://tycho.usno.navy.mil/cgi-bin/timer.pl
 /// and then prints the current UTC time by extracting just the UTC time from the web page's HTML.
-use std::io::prelude::*;
 
-fn get_page_html(url: &str) -> Result<String, Box<dyn Error>> {
-    let mut response = reqwest::get(url)?;
-    let mut response_text = String::new();
-    response.read_to_string(&mut response_text)?;
-    Ok(response_text)
-}
-fn main() -> Result<(), Box<dyn Error>> {
-    let raw_html = get_page_html("http://tycho.usno.navy.mil/cgi-bin/timer.pl")?;
+fn extract_timestamp(raw_html: &str) -> Option<String> {
     // parse the response body by <BR> tags
     let mut lines = raw_html
         .split("<BR>")
@@ -18,9 +10,18 @@ fn main() -> Result<(), Box<dyn Error>> {
             Some(_) => true,
             None => false,
         })
-        .map(|line| line.trim_end_matches("UTC\t\tUniversal Time\n").trim());
+        .map(|line| {
+            line.trim()
+                .replace("UTC\t\tUniversal Time", "")
+                .trim()
+                .to_string()
+        });
+    lines.next()
+}
 
-    if let Some(timestamp) = lines.next() {
+fn main() -> Result<(), Box<dyn Error>> {
+    let raw_html = reqwest::get("http://tycho.usno.navy.mil/cgi-bin/timer.pl")?.text()?;
+    if let Some(timestamp) = extract_timestamp(&raw_html) {
         print!("{}", timestamp);
     } else {
         eprint!("Error: Could not parse URL for getting current time.");
@@ -31,11 +32,26 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_page_html};
-    use std::error::Error;
+    use crate::extract_timestamp;
 
     #[test]
-    fn test_get_html() {
-        assert_eq!(Error::new("Something"), get_page_html("bad-url"));
+    fn test_extract_timestamp() {
+        let body = r#"<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final"//EN>
+            <html>
+            <body>
+            <TITLE>What time is it?</TITLE>
+            <H2> US Naval Observatory Master Clock Time</H2> <H3><PRE>
+            <BR>Aug. 12, 23:55:08 UTC		Universal Time
+            <BR>Aug. 12, 07:55:08 PM EDT		Eastern Time
+            <BR>Aug. 12, 06:55:08 PM CDT		Central Time
+            <BR>Aug. 12, 05:55:08 PM MDT		Mountain Time
+            <BR>Aug. 12, 04:55:08 PM PDT		Pacific Time
+            <BR>Aug. 12, 03:55:08 PM AKDT	Alaska Time
+            <BR>Aug. 12, 01:55:08 PM HAST	Hawaii-Aleutian Time
+            </PRE></H3><P><A HREF="http://www.usno.navy.mil"> US Naval Observatory</A>
+
+            </body></html>
+            "#;
+        assert_eq!(extract_timestamp(&body).unwrap(), "Aug. 12, 23:55:08");
     }
 }
