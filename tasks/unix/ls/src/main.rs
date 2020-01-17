a7daef9944787e7fdc84e3547a848c13d10b9984
Eli Flanagan <efx@users.noreply.github.com>
2020-01-16 20:30:34 -0500

    allow for ? operator
    Rust 2018 lets you use this when defining the main return signature
    accordingly.

--- tasks/unix/ls/src/main.rs
+++ tasks/unix/ls/src/main.rs
@@ -4,26 +4,27 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     // ignoring all arguments except the 1st
     match env::args().nth(1) {
         // check if the program received an argument
         Some(path) => {
-            print_files(Path::new(&path));
+            print_files(Path::new(&path))?;
         }
-        _ => {
-            print_files(&env::current_dir().unwrap());
+        None => {
+            print_files(&env::current_dir()?)?;
         } // note that current_dir value might be invalid, so it's a Result
-    }
+    };
+    Ok(())
 }
 
-fn print_files(path: &Path) {
-    let mut entries: Vec<_> = fs::read_dir(path)
-        .unwrap()
+fn print_files(path: &Path) -> std::io::Result<()> {
+    let mut entries: Vec<_> = fs::read_dir(path)?
         .map(|x| x.unwrap().file_name())
         .collect();
     entries.sort();
     for x in entries {
         println!("{}", x.to_string_lossy());
     }
+    Ok(())
 }
