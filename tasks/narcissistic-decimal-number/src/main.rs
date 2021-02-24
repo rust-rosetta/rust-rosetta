f4f7b861e737cec37860bd39a2519c7c5761ae44
Peter Budai <peterbudai@hotmail.com>
2021-02-24 22:04:40 +0100

    Implement narcissistic decimal number

---
+++ tasks/narcissistic-decimal-number/src/main.rs
@@ -0,0 +1,54 @@
+fn is_narc(x: u32) -> bool {
+    let digits: Vec<u32> = x
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    digits
+        .iter()
+        .map(|d| d.pow(digits.len() as u32))
+        .sum::<u32>()
+        == x
+}
+
+fn main() {
+    let mut counter = 0;
+    let mut i = 0;
+    while counter < 25 {
+        if is_narc(i) {
+            println!("{}", i);
+            counter += 1;
+        }
+        i += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::is_narc;
+    #[test]
+    fn test_is_narc() {
+        let input = 0;
+        let output = is_narc(input);
+        assert_eq!(output, true);
+
+        let input = 1;
+        let output = is_narc(input);
+        assert_eq!(output, true);
+
+        let input = 153;
+        let output = is_narc(input);
+        assert_eq!(output, true);
+    }
+    #[test]
+    fn test_not_is_narc() {
+        let input = 10;
+        let output = is_narc(input);
+        assert_eq!(output, false);
+
+        let input = 999999999;
+        let output = is_narc(input);
+        assert_eq!(output, false);
+    }
+}
