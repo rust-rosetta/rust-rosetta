f52ceadb5efa39ebfb67986c873741c5d5fcee48
Peter Budai <peterbudai@hotmail.com>
2021-04-03 22:06:09 +0200

    Implement Sequence of primes by trial division

---
+++ tasks/sequence-of-primes-by-trial-division/src/main.rs
@@ -0,0 +1,19 @@
+fn is_prime(number: u32) -> bool {
+    #[allow(clippy:clippy::cast_precision_loss)]
+    let limit = (number as f32).sqrt() as u32 + 1;
+
+    // We test if the number is divisible by any number up to the limit
+    !(number < 2 || (2..limit).any(|x| number % x == 0))
+}
+
+fn main() {
+    println!(
+        "Primes below 100:\n{:?}",
+        (0_u32..100).fold(vec![], |mut acc, number| {
+            if is_prime(number) {
+                acc.push(number)
+            };
+            acc
+        })
+    );
+}
