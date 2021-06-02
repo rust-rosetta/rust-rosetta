e7b5496c62cda5019af658207f8ddb4171088511
Avrosh K <avroshkumar@gmail.com>
2021-06-02 01:53:41 -0400

    Added docstring

--- tasks/statistics/normal-distribution/src/main.rs
+++ tasks/statistics/normal-distribution/src/main.rs
@@ -1,27 +1,40 @@
+//! Rust rosetta example for normal distribution
 use rand;
 use rand_distr::{Normal,Distribution};
 use math::{histogram::Histogram, traits::ToIterator};
 
+/// Returns the mean of the provided samples
+///
+/// ## Arguments
+/// * data -- reference to float32 array
 fn mean(data: &[f32]) -> Option<f32> {
     let sum: f32 = data.iter().sum();
     Some(sum / data.len() as f32)
 }
 
+/// Returns standard deviation of the provided samples
+///
+/// ## Arguments
+/// * data -- reference to float32 array
 fn standard_deviation(data: &[f32]) -> Option<f32> {
     let mean = mean(data).expect("invalid mean");
     let sum = data.iter().fold(0.0, |acc, &x| acc + (x - mean).powi(2));
     Some((sum / data.len() as f32).sqrt())
 }
 
+/// Prints a histogram in the shell
+///
+/// ## Arguments
+/// * data -- reference to float32 array
+/// * maxwidth -- the maxwidth of the histogram in # of characters
+/// * bincount -- number of bins in the histogram
+/// * ch -- character used to plot the graph
 fn print_histogram(data: &[f32], maxwidth: usize, bincount: usize, ch: char) {
-    // maxwidth -- the maxwidth of the histogram in # of characters
-    // bincount -- number of bins in the histogram
-    // ch -- character used to plot the graph
     let min_val = data.iter().cloned().fold(f32::NAN, f32::min);
     let max_val = data.iter().cloned().fold(f32::NAN, f32::max);
     let histogram = Histogram::new(Some(&data.to_vec()), bincount, min_val, max_val).unwrap();
     let max_bin_value = histogram.get_counters().iter().max().unwrap();
-    println!("");
+    println!();
     for x in histogram.to_iter() {
         let (bin_min, bin_max, freq) = x;
         let bar_width = (((freq as f64)/(*max_bin_value as f64))*(maxwidth as f64)) as u32;
@@ -32,9 +45,10 @@ fn print_histogram(data: &[f32], maxwidth: usize, bincount: usize, ch: char) {
                  bar_as_string,
                  (freq as f64)*100.0/(data.len() as f64));
     }
-    println!("");
+    println!();
 }
 
+/// Runs the demo to generate normal distribution of three different sample sizes
 fn main() {
     let expected_mean: f32 = 0.0;
     let expected_std_deviation: f32 = 4.0;
