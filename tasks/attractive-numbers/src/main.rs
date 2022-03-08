dff22ad6f3b2edd02a482e6ef24fb51ded7cdb6f
Abror Xalilov <a.xalilov1997@gmail.com>
2022-03-08 11:54:16 +0500

    optimize calculation

--- tasks/attractive-numbers/src/main.rs
+++ tasks/attractive-numbers/src/main.rs
@@ -1,16 +1,18 @@
-use primal::Primes;
+use primal::StreamingSieve;
 
 const MAX: u64 = 120;
 
 /// Returns an Option with a tuple => Ok((smaller prime factor, num divided by that prime factor))
 /// If num is a prime number itself, returns None
 fn extract_prime_factor(num: u64) -> Option<(u64, u64)> {
-    let mut i = 0;
-    if primal::is_prime(num) {
+    let mut i = 1;
+
+    if num < 2 || primal::is_prime(num) {
         None
     } else {
         loop {
-            let prime = Primes::all().nth(i).unwrap() as u64;
+            let prime = StreamingSieve::nth_prime(i) as u64;
+
             if num % prime == 0 {
                 return Some((prime, num / prime));
             } else {
@@ -24,10 +26,12 @@ fn extract_prime_factor(num: u64) -> Option<(u64, u64)> {
 fn factorize(num: u64) -> Vec<u64> {
     let mut factorized = Vec::new();
     let mut rest = num;
+
     while let Some((prime, factorizable_rest)) = extract_prime_factor(rest) {
         factorized.push(prime);
         rest = factorizable_rest;
     }
+
     factorized.push(rest);
     factorized
 }
@@ -39,8 +43,9 @@ fn main() {
             output.push(num);
         }
     }
+
     println!(
-        "The attractive numbers up to and including 120 are\n{:?}",
-        output
-    );
+        "The attractive numbers up to and including {} are\n{:?}",
+        MAX, output
+    )
 }
