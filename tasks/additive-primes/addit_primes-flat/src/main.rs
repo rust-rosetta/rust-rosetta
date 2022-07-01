3b5ae326f507c546bc33ede2d04ca81e941d7884
Vladimir Shardinov <vvshard@gmail.com>
2022-07-01 16:16:28 +0300

    refactoring additive-primes

--- tasks/additive-primes/addit_primes-flat/src/main.rs
+++ tasks/additive-primes/addit_primes-flat/src/main.rs
@@ -2,26 +2,18 @@
 
 fn main() {
     let limit = 500;
-    //all primes, starting from 3 (including non-additive), will be collected in pms
-    //  it works ~1.5 times faster than the variant with fn is_prime(){...while...+=6...}
+    let column_w = limit.to_string().len() + 1;
     let mut pms = Vec::with_capacity(limit / 2 - limit / 3 / 2 - limit / 5 / 3 / 2 + 1);
-    let column_width = limit.to_string().len() + 1;
-    print!("{:1$}", 2, column_width);
-    let mut count = 1;
-    for u in (3..limit).step_by(2) {
+    let mut count = 0;
+    for u in (2..3).chain((3..limit).step_by(2)) {
         if pms.iter().take_while(|&&p| p * p <= u).all(|&p| u % p != 0) {
             pms.push(u);
-            //about the same speed as while...{...+=...%.../=...}, but without mut
-            let sum_digits = std::iter::successors(Some(u), |&n| (n > 9).then(|| n / 10))
-                .fold(0, |s, n| s + n % 10);
-            if sum_digits == 2 || matches!(pms.binary_search(&sum_digits), Ok(_)) {
-                if count % 10 == 0 {
-                    println!();
-                }
-                print!("{:1$}", u, column_width);
+            let dgs = std::iter::successors(Some(u), |&n| (n > 9).then(|| n / 10)).map(|n| n % 10);
+            if pms.binary_search(&dgs.sum()).is_ok() {
+                print!("{}{u:column_w$}", if count % 10 == 0 { "\n" } else { "" });
                 count += 1;
             }
         }
     }
-    println!("\n---\nFound {} additive primes less than {}", count, limit);
+    println!("\n---\nFound {count} additive primes less than {limit}");
 }
