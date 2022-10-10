1623df534f7e226b1e478a99de48b5c95c0ccbfc
Johanna Sörngård <johanna.sorngard@fysik.su.se>
2022-10-10 16:19:27 +0200

    Added tests

--- tasks/harmonic-series/src/main.rs
+++ tasks/harmonic-series/src/main.rs
@@ -8,23 +8,23 @@ fn main() {
 
     println!("Harmonic number 100 = {}", harmonic_number(100));
 
-    //In order to avoid recomputing all the terms in the sum for the n:th harmonic number
+    //In order to avoid recomputing all the terms in the sum for every harmonic number
     //we save the value of the harmonic series between loop iterations
     //and just add 1/iter to it.
 
     let mut target = 1;
     let mut iter = 1;
-    let mut h: Ratio<BigInt> = Ratio::from_integer(1.into());
+    let mut harmonic_number: Ratio<BigInt> = Ratio::from_integer(1.into());
 
     while target <= 10 {
-        if h > Ratio::from_integer(target.into()) {
+        if harmonic_number > Ratio::from_integer(target.into()) {
             println!("Position of first term > {target} is {iter}");
             target += 1;
         }
 
         //Compute the next term in the harmonic series
         iter += 1;
-        h += Ratio::from_integer(iter.into()).recip();
+        harmonic_number += Ratio::from_integer(iter.into()).recip();
     }
 }
 
@@ -32,4 +32,47 @@ fn harmonic_number(n: u64) -> Ratio<BigInt> {
     //Convert each integer from 1 to n into an arbitrary precision rational number
     //and sum their reciprocals
     (1..=n).map(|i| Ratio::from_integer(i.into()).recip()).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn twenty_first() {
+        const CORRECT_FIRST_TWENTY: [[u64; 2]; 20] = [
+            [1, 1],
+            [3, 2],
+            [11, 6],
+            [25, 12],
+            [137, 60],
+            [49, 20],
+            [363, 140],
+            [761, 280],
+            [7129, 2520],
+            [7381, 2520],
+            [83711, 27720],
+            [86021, 27720],
+            [1145993, 360360],
+            [1171733, 360360],
+            [1195757, 360360],
+            [2436559, 720720],
+            [42142223, 12252240],
+            [14274301, 4084080],
+            [275295799, 77597520],
+            [55835135, 15519504],
+        ];
+        for (i, [numerator, denominator]) in CORRECT_FIRST_TWENTY.into_iter().enumerate() {
+            assert_eq!(
+                harmonic_number((i + 1).try_into().unwrap()),
+                Ratio::new(numerator.into(), denominator.into())
+            );
+        }
+
+        let correct_hundred = Ratio::new(
+            BigInt::parse_bytes(b"14466636279520351160221518043104131447711", 10).unwrap(),
+            BigInt::parse_bytes(b"2788815009188499086581352357412492142272", 10).unwrap(),
+        );
+        assert_eq!(harmonic_number(100), correct_hundred);
+    }
 }
